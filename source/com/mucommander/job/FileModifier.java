88f1cde4001c989649e0fe71d97e0e1ed2bac443
Maxence Bernard <maxence@maxence.info>
2004-01-06 23:11:55 +0000

    *** empty log message ***

---
+++ source/com/mucommander/job/FileModifier.java
@@ -0,0 +1,15 @@
+
+package com.mucommander.job;
+
+
+/**
+ * This interface should be impletement by FileJob classes that alter files
+ * which may be visible in a folder's view.
+ *
+ * <p>This interface is used by FileJob to determine if it should pause tables'
+ * auto-refresh during the job.</p>
+ *
+ * @author Maxence Bernard
+ */
+public interface FileModifier {
+}
