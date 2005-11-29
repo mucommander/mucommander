6fbd9059cc30b7cffce3616c98708edb15310655
Maxence Bernard <maxence@maxence.info>
2005-11-29 22:25:56 +0000

    *** empty log message ***

--- source/com/mucommander/Launcher.java
+++ source/com/mucommander/Launcher.java
@@ -106,6 +106,11 @@ public class Launcher {
         if(ConfigurationManager.getVariable("prefs.check_for_updates_on_startup", "true").equals("true"))
             new CheckVersionDialog(WindowManager.getInstance().getCurrentMainFrame(), false);
 		
+		// Silence jCIFS's output if not in debug mode
+		// To quote jCIFS's documentation : "0 - No log messages are printed -- not even crticial exceptions."
+		if(!com.mucommander.Debug.ON)
+			System.setProperty("jcifs.util.loglevel", "0");
+		
 		// Dispose splash screen
 		splashScreen.dispose();
 	}
