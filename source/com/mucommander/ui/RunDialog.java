b741be257848669a585c37d098140d30d82be777
Nicolas Rinaudo <nicolas@nrinaudo.com>
2006-04-27 10:00:18 +0000

    Changed the focus behavior: command line will now request focus after a command has been executed. Command output is now wrapped.

--- source/com/mucommander/ui/RunDialog.java
+++ source/com/mucommander/ui/RunDialog.java
@@ -22,62 +22,63 @@ import javax.swing.*;
  * @author Maxence Bernard
  */
 public class RunDialog extends FocusDialog implements ActionListener, ProcessListener {
-	private MainFrame mainFrame;
+    private MainFrame mainFrame;
 	
-	private JTextField commandField;
+    private JTextField commandField;
 	
-	private JButton runStopButton;
-	private JButton cancelButton;
+    private JButton runStopButton;
+    private JButton cancelButton;
 
-	private JTextArea outputTextArea;
+    private JTextArea outputTextArea;
 	
-	private Process currentProcess;
-	private ProcessMonitor processMonitor;
+    private Process currentProcess;
+    private ProcessMonitor processMonitor;
 	
-	// Dialog size constraints
-	private final static Dimension MINIMUM_DIALOG_DIMENSION = new Dimension(480, 400);	
-//    // Dialog width should not exceed 360
-//   private final static Dimension MAXIMUM_DIALOG_DIMENSION = new Dimension(360,10000);	
+    // Dialog size constraints
+    private final static Dimension MINIMUM_DIALOG_DIMENSION = new Dimension(480, 400);	
+    //    // Dialog width should not exceed 360
+    //   private final static Dimension MAXIMUM_DIALOG_DIMENSION = new Dimension(360,10000);	
 
-	private static String lastCommand = "";
+    private static String lastCommand = "";
 
-	private int caretPos;
+    private int caretPos;
 	
-	/**
-	 * Creates and displays a new RunDialog.
-	 * 
-	 * @param mainFrame the main frame this dialog is attached to.
-	 */
-	public RunDialog(MainFrame mainFrame) {
-		super(mainFrame, Translator.get("run_dialog.run_command"), mainFrame);
-		this.mainFrame = mainFrame;
+    /**
+     * Creates and displays a new RunDialog.
+     * 
+     * @param mainFrame the main frame this dialog is attached to.
+     */
+    public RunDialog(MainFrame mainFrame) {
+        super(mainFrame, Translator.get("run_dialog.run_command"), mainFrame);
+        this.mainFrame = mainFrame;
 		
-		Container contentPane = getContentPane();
+        Container contentPane = getContentPane();
         YBoxPanel mainPanel = new YBoxPanel();
 		
-		JLabel label = new JLabel(Translator.get("run_dialog.run_command_description")+":");
+        JLabel label = new JLabel(Translator.get("run_dialog.run_command_description")+":");
         mainPanel.add(label);
 
-		commandField = new JTextField(lastCommand);
+        commandField = new JTextField(lastCommand);
         // Text is selected so that user can directly type and replace path
         commandField.setSelectionStart(0);
         commandField.setSelectionEnd(lastCommand.length());
         commandField.addActionListener(this);
 		
-		mainPanel.add(commandField);
-		mainPanel.addSpace(10);
+        mainPanel.add(commandField);
+        mainPanel.addSpace(10);
 
         contentPane.add(mainPanel, BorderLayout.NORTH);
 
-		mainPanel.add(new JLabel(Translator.get("run_dialog.command_output")+":"));
-		outputTextArea = new JTextArea();
-		outputTextArea.setRows(10);
-		outputTextArea.setEditable(false);
-		JScrollPane scrollPane = new JScrollPane(outputTextArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        mainPanel.add(new JLabel(Translator.get("run_dialog.command_output")+":"));
+        outputTextArea = new JTextArea();
+        outputTextArea.setLineWrap(true);
+        outputTextArea.setRows(10);
+        outputTextArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(outputTextArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-		contentPane.add(scrollPane, BorderLayout.CENTER);
+        contentPane.add(scrollPane, BorderLayout.CENTER);
 		
-		// Run / Cancel buttons panel
+        // Run / Cancel buttons panel
         runStopButton = new JButton(Translator.get("run_dialog.run"));
         cancelButton = new JButton(Translator.get("cancel"));
         contentPane.add(DialogToolkit.createOKCancelPanel(runStopButton, cancelButton, this), BorderLayout.SOUTH);
@@ -85,110 +86,113 @@ public class RunDialog extends FocusDialog implements ActionListener, ProcessLis
         // Selects OK when enter is pressed
         getRootPane().setDefaultButton(runStopButton);
         
-		// Path field will receive initial focus
-		setInitialFocusComponent(commandField);		
+        // Path field will receive initial focus
+        setInitialFocusComponent(commandField);		
 			
-		setMinimumSize(MINIMUM_DIALOG_DIMENSION);
-//		setMaximumSize(MAXIMUM_DIALOG_DIMENSION);
+        setMinimumSize(MINIMUM_DIALOG_DIMENSION);
+        //		setMaximumSize(MAXIMUM_DIALOG_DIMENSION);
 
-		// Closing this dialog kills the process
-		addWindowListener(new WindowAdapter() {
-			public void windowClosed(WindowEvent e) {
-				if(currentProcess!=null) {
-					currentProcess.destroy();
-					processMonitor.stopMonitoring();
-				}
-			}
-		});
+        // Closing this dialog kills the process
+        addWindowListener(new WindowAdapter() {
+                public void windowClosed(WindowEvent e) {
+                    if(currentProcess!=null) {
+                        currentProcess.destroy();
+                        processMonitor.stopMonitoring();
+                    }
+                }
+            });
 		
-		showDialog();
-	}
+        showDialog();
+    }
 
 	
-	public void processDied(Process process, int retValue) {
-if(com.mucommander.Debug.ON) com.mucommander.Debug.trace("process "+process+" exit, return value= "+retValue);
-		this.currentProcess = null;
-		switchToRunState();
-	}	
+    public void processDied(Process process, int retValue) {
+        if(com.mucommander.Debug.ON) com.mucommander.Debug.trace("process "+process+" exit, return value= "+retValue);
+        this.currentProcess = null;
+        switchToRunState();
+    }	
 
 	
-	public void processOutput(Process process, byte buffer[], int offset, int length) {
-if(com.mucommander.Debug.ON) com.mucommander.Debug.trace("processOutput "+process+" output= "+new String(buffer, 0, length));
-		addToTextArea(buffer, offset, length);
-	}
+    public void processOutput(Process process, byte buffer[], int offset, int length) {
+        if(com.mucommander.Debug.ON) com.mucommander.Debug.trace("processOutput "+process+" output= "+new String(buffer, 0, length));
+        addToTextArea(buffer, offset, length);
+    }
 
 	
-	public void processError(Process process, byte buffer[], int offset, int length) {
-if(com.mucommander.Debug.ON) com.mucommander.Debug.trace("processError "+process+" output= "+new String(buffer, 0, length));
-		addToTextArea(buffer, offset, length);
-	}
+    public void processError(Process process, byte buffer[], int offset, int length) {
+        if(com.mucommander.Debug.ON) com.mucommander.Debug.trace("processError "+process+" output= "+new String(buffer, 0, length));
+        addToTextArea(buffer, offset, length);
+    }
 
 
-	private void addToTextArea(byte buffer[], int offset, int length) {
-		outputTextArea.append(new String(buffer, 0, length));
-		caretPos += length;
-		outputTextArea.setCaretPosition(caretPos);
-		outputTextArea.repaint();
-	}
+    private void addToTextArea(byte buffer[], int offset, int length) {
+        outputTextArea.append(new String(buffer, 0, length));
+        caretPos += length;
+        outputTextArea.setCaretPosition(caretPos);
+        outputTextArea.repaint();
+    }
 	
 	
-	private void switchToStopState() {
-		// Change 'Run' button to 'Stop'
-		this.runStopButton.setText(Translator.get("run_dialog.stop"));
-		// Clear text area
-		this.outputTextArea.setText("");
-//				// Make text area active
-//				this.outputTextArea.setEnabled(true);
-		// Make command field disabled
-		this.commandField.setEnabled(false);
-		// Repaint the dialog
-		repaint();
-	}
+    private void switchToStopState() {
+        // Change 'Run' button to 'Stop'
+        this.runStopButton.setText(Translator.get("run_dialog.stop"));
+        // Clear text area
+        this.outputTextArea.setText("");
+        //				// Make text area active
+        //				this.outputTextArea.setEnabled(true);
+        // Make command field disabled
+        this.commandField.setEnabled(false);
+        // Repaint the dialog
+        repaint();
+    }
 	
-	private void switchToRunState() {
-		// Change 'Stop' button to 'Run'
-		this.runStopButton.setText(Translator.get("run_dialog.run"));
-//		// Make text area not active anymore
-//		this.outputTextArea.setEnabled(false);
-		// Make command field active again
-		this.commandField.setEnabled(true);
-		// Repaint this dialog
-		repaint();
-	}	
+    private void switchToRunState() {
+        // Change 'Stop' button to 'Run'
+        this.runStopButton.setText(Translator.get("run_dialog.run"));
+        //		// Make text area not active anymore
+        //		this.outputTextArea.setEnabled(false);
+        // Make command field active again
+        this.commandField.setEnabled(true);
+        commandField.requestFocus();
+        commandField.setSelectionStart(0);
+        commandField.setSelectionEnd(lastCommand.length());
+        // Repaint this dialog
+        repaint();
+    }	
 
 
-	////////////////////////////
-	// ActionListener methods //
-	////////////////////////////
+    ////////////////////////////
+    // ActionListener methods //
+    ////////////////////////////
 	
-	public void actionPerformed(ActionEvent e) {
-		Object source = e.getSource();
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
 		
-		// Run button starts a new command
-		if(this.currentProcess==null && (source == runStopButton || source == commandField)) {
-			String command = commandField.getText();
-			this.lastCommand = command;
-			this.currentProcess = PlatformManager.execute(command, mainFrame.getLastActiveTable().getCurrentFolder());
-			// If command could be executed
-			if(currentProcess!=null) {
-				// Reset caret position
-				caretPos = 0;
-				switchToStopState();
-				// And start monitoring the process and outputting to the text area
-				processMonitor = new ProcessMonitor(this.currentProcess, this);
-			}
-			// Probably should notify the user if the command could not be executed
-		}
-		// Stop button stops current process
-		else if(this.currentProcess!=null && source==runStopButton) {
-			processMonitor.stopMonitoring();
-			currentProcess.destroy();
-			this.currentProcess = null;
-			switchToRunState();
-		}
-		// Cancel button disposes the dialog and kills the process
-		else if(source == cancelButton) {
-			dispose();			
-		}
-	}	
+        // Run button starts a new command
+        if(this.currentProcess==null && (source == runStopButton || source == commandField)) {
+            String command = commandField.getText();
+            this.lastCommand = command;
+            this.currentProcess = PlatformManager.execute(command, mainFrame.getLastActiveTable().getCurrentFolder());
+            // If command could be executed
+            if(currentProcess!=null) {
+                // Reset caret position
+                caretPos = 0;
+                switchToStopState();
+                // And start monitoring the process and outputting to the text area
+                processMonitor = new ProcessMonitor(this.currentProcess, this);
+            }
+            // Probably should notify the user if the command could not be executed
+        }
+        // Stop button stops current process
+        else if(this.currentProcess!=null && source==runStopButton) {
+            processMonitor.stopMonitoring();
+            currentProcess.destroy();
+            this.currentProcess = null;
+            switchToRunState();
+        }
+        // Cancel button disposes the dialog and kills the process
+        else if(source == cancelButton) {
+            dispose();			
+        }
+    }	
 }
