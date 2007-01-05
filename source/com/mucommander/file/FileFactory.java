1f5cb7b7f920f3c79bd71a86b51d7d674046aa9b
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-01-05 14:12:37 +0000

    *** empty log message ***

--- source/com/mucommander/file/FileFactory.java
+++ source/com/mucommander/file/FileFactory.java
@@ -62,7 +62,7 @@ public abstract class FileFactory {
         
         // Register built-in archive file formats, order for TarArchiveFile and GzipArchiveFile/Bzip2ArchiveFile is important:
         // TarArchiveFile must match 'tar.gz'/'tar.bz2' files before GzipArchiveFile/Bzip2ArchiveFile does.
-        registerArchiveFileFormat(ZipArchiveFile.class, new ExtensionFilenameFilter(new String[]{".zip", ".jar", ".pk3", ".pk4", ".war", ".wal", ".smzip", ".wmz", ".xpi", ".ear", ".sar"}));
+        registerArchiveFileFormat(ZipArchiveFile.class, new ExtensionFilenameFilter(new String[]{".zip", ".jar", ".pk3", ".pk4", ".war", ".wal", ".wmz", ".xpi", ".ear", ".sar"}));
         registerArchiveFileFormat(TarArchiveFile.class, new ExtensionFilenameFilter(new String[]{".tar", ".tar.gz", ".tgz", ".tar.bz2", ".tbz2"}));
         registerArchiveFileFormat(GzipArchiveFile.class, new ExtensionFilenameFilter(".gz"));
         registerArchiveFileFormat(Bzip2ArchiveFile.class, new ExtensionFilenameFilter(".bz2"));
