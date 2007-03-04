640f43688f02792ec8236213f4b077126128e8ae
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-03-04 01:06:50 +0000

    Woops, he he he...

---
+++ source/com/mucommander/command/AssociationFactory.java
@@ -0,0 +1,33 @@
+package com.mucommander.command;
+
+import com.mucommander.file.filter.*;
+
+/**
+ * @author Nicolas Rinaudo
+ */
+class AssociationFactory implements AssociationBuilder {
+    private AndFileFilter filter;
+    private String        command;
+
+    public void startBuilding() {}
+    public void endBuilding() {}
+
+    public void startAssociation(String command) {
+        filter       = new AndFileFilter();
+        this.command = command;
+    }
+
+    public void endAssociation() throws CommandException {
+        if(filter.isEmpty())
+            throw new CommandException("Empty association file filter.");
+        CommandManager.registerAssociation(command, filter);
+    }
+
+    public void setMask(String mask, boolean isCaseSensitive) {filter.addFileFilter(new RegexpFilenameFilter(mask, isCaseSensitive));}
+    public void setIsDir(boolean isDir) {filter.addFileFilter(new TypeFileFilter(TypeFileFilter.DIRECTORY, isDir));}
+    public void setIsSymlink(boolean isSymlink) {filter.addFileFilter(new TypeFileFilter(TypeFileFilter.SYMLINK, isSymlink));}
+    public void setIsHidden(boolean isHidden) {filter.addFileFilter(new TypeFileFilter(TypeFileFilter.HIDDEN, isHidden));}
+    public void setIsReadable(boolean isReadable) {filter.addFileFilter(new PermissionsFileFilter(PermissionsFileFilter.READ_PERMISSION, isReadable));}
+    public void setIsWritable(boolean isWritable) {filter.addFileFilter(new PermissionsFileFilter(PermissionsFileFilter.WRITE_PERMISSION, isWritable));}
+    public void setIsExecutable(boolean isExecutable) {filter.addFileFilter(new PermissionsFileFilter(PermissionsFileFilter.EXECUTE_PERMISSION, isExecutable));}
+}
