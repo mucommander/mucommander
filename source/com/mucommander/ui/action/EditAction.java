8aec0e27fde650b5db83752e809d8d3190e2523c
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-04-03 13:42:49 +0000

    Now use configuration options to spawn a custom process.

--- source/com/mucommander/ui/action/EditAction.java
+++ source/com/mucommander/ui/action/EditAction.java
@@ -1,29 +1,103 @@
 package com.mucommander.ui.action;
 
-import com.mucommander.file.AbstractFile;
-import com.mucommander.file.filter.DirectoryFileFilter;
+import com.mucommander.file.*;
+import com.mucommander.file.impl.local.LocalFile;
+import com.mucommander.ui.ProgressDialog;
+import com.mucommander.text.Translator;
+import com.mucommander.job.TempOpenWithJob;
 import com.mucommander.ui.MainFrame;
-import com.mucommander.ui.editor.EditorRegistrar;
+import com.mucommander.conf.*;
+import com.mucommander.command.*;
+import com.mucommander.process.*;
 
 import java.util.Hashtable;
 
 /**
- * This action opens the currently selected file in an integrated editor.
- *
- * @author Maxence Bernard
+ * Customisable version of {@link InternalEditAction}.
+ * @author Maxence Bernard, Nicolas Rinaudo
  */
-public class EditAction extends SelectedFileAction {
+public class EditAction extends InternalEditAction implements ConfigurationListener {
+    // - Instance variables --------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /** Custom editor defined in the configuration. */
+    private Command customEditor;
+    /** Whether or not to use the custom editor. */
+    private boolean useCustomEditor;
 
+
+
+    // - Initialisation ------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /**
+     * Creates a new <code>EditAction</code>.
+     */
     public EditAction(MainFrame mainFrame, Hashtable properties) {
         super(mainFrame, properties);
 
-        // Only enable this action if currently selected file is not a directory
-        setSelectedFileFilter(new DirectoryFileFilter());
+        // Initialises configuration.
+        useCustomEditor = ConfigurationManager.getVariableBoolean(ConfigurationVariables.USE_CUSTOM_EDITOR, ConfigurationVariables.DEFAULT_USE_CUSTOM_EDITOR);
+        setCustomEditor(ConfigurationManager.getVariable(ConfigurationVariables.CUSTOM_EDITOR));
     }
 
-    public void performAction() {
-        AbstractFile file = mainFrame.getActiveTable().getSelectedFile();
-        if(file!=null && !(file.isDirectory() || file.isSymlink()))
-            EditorRegistrar.createEditorFrame(mainFrame, file, getIcon().getImage());
+
+
+    // - Action execution ----------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /**
+     * Edits the currently selected file.
+     */
+    public synchronized void performAction() {
+        // If we're using a custom editor...
+        if(useCustomEditor) {
+            AbstractFile file;
+
+            file = mainFrame.getActiveTable().getSelectedFile();
+            // If the file is editable...
+            if(file != null && !(file.isDirectory() || file.isSymlink())) {
+                // If it's local, run the custom editor on it.
+                if(file.getURL().getProtocol().equals(FileProtocols.FILE) && (file instanceof LocalFile)) {
+                    try {ProcessRunner.execute(customEditor.getTokens(file), file);}
+                    catch(Exception e) {}
+                }
+                // If it's distant, copies it locally before running the custom editor on it.
+                else {
+                    ProgressDialog progressDialog = new ProgressDialog(mainFrame, Translator.get("copy_dialog.copying"));
+                    TempOpenWithJob job = new TempOpenWithJob(progressDialog, mainFrame, file, FileFactory.getTemporaryFile(file.getName(), true), customEditor);
+                    progressDialog.start(job);
+                }
+            }
+        }
+        // If we're not using a custom editor, this action behaves exactly like its parent.
+        else
+            super.performAction();
+    }
+
+
+
+    // - Configuration management --------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /**
+     * Reacts to configuration changed events.
+     * @param event describes the configuration change.
+     */
+    public synchronized boolean configurationChanged(ConfigurationEvent event) {
+        // Updates useCustomEditor.
+        if(event.getVariable().equals(ConfigurationVariables.USE_CUSTOM_EDITOR))
+            useCustomEditor = event.getBooleanValue();
+        // Updates customEditor.
+        else if(event.getVariable().equals(ConfigurationVariables.CUSTOM_EDITOR))
+            setCustomEditor(event.getValue());
+        return true;
+    }
+
+    /**
+     * Sets the custom editor to the specified command.
+     * @param command command to use as a custom editor.
+     */
+    private void setCustomEditor(String command) {
+        if(command == null)
+            customEditor = null;
+        else
+            customEditor = CommandParser.getCommand("edit", command);
     }
 }
--- source/com/mucommander/ui/action/ViewAction.java
+++ source/com/mucommander/ui/action/ViewAction.java
@@ -1,29 +1,103 @@
 package com.mucommander.ui.action;
 
-import com.mucommander.file.AbstractFile;
-import com.mucommander.file.filter.DirectoryFileFilter;
+import com.mucommander.file.*;
+import com.mucommander.file.impl.local.LocalFile;
+import com.mucommander.ui.ProgressDialog;
+import com.mucommander.text.Translator;
+import com.mucommander.job.TempOpenWithJob;
 import com.mucommander.ui.MainFrame;
-import com.mucommander.ui.viewer.ViewerRegistrar;
+import com.mucommander.conf.*;
+import com.mucommander.command.*;
+import com.mucommander.process.*;
 
 import java.util.Hashtable;
 
 /**
- * This action opens the currently selected file in an integrated viewer.
- *
- * @author Maxence Bernard
+ * Customisable version of {@link InternalViewAction}.
+ * @author Maxence Bernard, Nicolas Rinaudo
  */
-public class ViewAction extends SelectedFileAction {
+public class ViewAction extends InternalViewAction implements ConfigurationListener {
+    // - Instance variables --------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /** Custom viewer defined in the configuration. */
+    private Command customViewer;
+    /** Whether or not to use the custom viewer. */
+    private boolean useCustomViewer;
 
+
+
+    // - Initialisation ------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /**
+     * Creates a new <code>ViewAction</code>.
+     */
     public ViewAction(MainFrame mainFrame, Hashtable properties) {
         super(mainFrame, properties);
 
-        // Only enable this action if currently selected file is not a directory
-        setSelectedFileFilter(new DirectoryFileFilter());
+        // Initialises configuration.
+        useCustomViewer = ConfigurationManager.getVariableBoolean(ConfigurationVariables.USE_CUSTOM_VIEWER, ConfigurationVariables.DEFAULT_USE_CUSTOM_VIEWER);
+        setCustomViewer(ConfigurationManager.getVariable(ConfigurationVariables.CUSTOM_VIEWER));
     }
 
-    public void performAction() {
-        AbstractFile file = mainFrame.getActiveTable().getSelectedFile();
-        if(file!=null && !(file.isDirectory() || file.isSymlink()))
-            ViewerRegistrar.createViewerFrame(mainFrame, file, getIcon().getImage());
+
+
+    // - Action execution ----------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /**
+     * Views the currently selected file.
+     */
+    public synchronized void performAction() {
+        // If we're using a custom viewer...
+        if(useCustomViewer) {
+            AbstractFile file;
+
+            file = mainFrame.getActiveTable().getSelectedFile();
+            // If the file is viewable...
+            if(file != null && !(file.isDirectory() || file.isSymlink())) {
+                // If it's local, run the custom viewer on it.
+                if(file.getURL().getProtocol().equals(FileProtocols.FILE) && (file instanceof LocalFile)) {
+                    try {ProcessRunner.execute(customViewer.getTokens(file), file);}
+                    catch(Exception e) {}
+                }
+                // If it's distant, copies it locally before running the custom viewer on it.
+                else {
+                    ProgressDialog progressDialog = new ProgressDialog(mainFrame, Translator.get("copy_dialog.copying"));
+                    TempOpenWithJob job = new TempOpenWithJob(progressDialog, mainFrame, file, FileFactory.getTemporaryFile(file.getName(), true), customViewer);
+                    progressDialog.start(job);
+                }
+            }
+        }
+        // If we're not using a custom viewer, this action behaves exactly like its parent.
+        else
+            super.performAction();
+    }
+
+
+
+    // - Configuration management --------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /**
+     * Reacts to configuration changed events.
+     * @param event describes the configuration change.
+     */
+    public synchronized boolean configurationChanged(ConfigurationEvent event) {
+        // Updates useCustomViewer.
+        if(event.getVariable().equals(ConfigurationVariables.USE_CUSTOM_VIEWER))
+            useCustomViewer = event.getBooleanValue();
+        // Updates customViewer.
+        else if(event.getVariable().equals(ConfigurationVariables.CUSTOM_VIEWER))
+            setCustomViewer(event.getValue());
+        return true;
+    }
+
+    /**
+     * Sets the custom viewer to the specified command.
+     * @param command command to use as a custom viewer.
+     */
+    private void setCustomViewer(String command) {
+        if(command == null)
+            customViewer = null;
+        else
+            customViewer = CommandParser.getCommand("view", command);
     }
 }
