f2d8ea43dc4439b079eae7b1843d4c975c5d0c04
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-04-09 01:39:49 +0000

    Not necessary anymore: the shell script is now generic, and the portable build uses a Win32 executable.

--- res/mucommander.sh
+++ res/mucommander.sh
@@ -1,9 +1,30 @@
 #! /bin/sh
-if [ "$JAVA_HOME" != "" ]; then
+
+# muCommander startup arguments
+MUCOMMANDER_ARGS="@ARGS@"
+
+# Locates the java executable.
+if [ "$JAVA_HOME" != "" ] ; then
     JAVA=$JAVA_HOME/bin/java
 else
-    echo JAVA_HOME environment variable not set, trying default java VM
-    JAVA=java
+    JAVACMD=`which java 2> /dev/null `
+    if [ -z "$JAVACMD" ] ; then
+        echo "Error: cannot find java VM."
+        exit 1
+    else
+        JAVA=java
+    fi
 fi
 
-$JAVA -DGNOME_DESKTOP_SESSION_ID=$GNOME_DESKTOP_SESSION_ID -DKDE_FULL_SESSION=$KDE_FULL_SESSION -jar mucommander.jar $@
+# Locates the mucommander jar file.
+if [ -f ./mucommander.jar ] ; then
+    MUCOMMANDER_JAR=./mucommander.jar
+elif [ -f /usr/share/mucommander/mucommander.jar ] ; then
+    MUCOMMANDER_JAR=/usr/share/mucommander/mucommander.jar
+else
+    echo "Error: cannot find mucommander.jar"
+    exit 1
+fi
+
+# Starts mucommander.
+$JAVA -DGNOME_DESKTOP_SESSION_ID=$GNOME_DESKTOP_SESSION_ID -DKDE_FULL_SESSION=$KDE_FULL_SESSION -jar $MUCOMMANDER_JAR $MUCOMMANDER_ARGS $@
