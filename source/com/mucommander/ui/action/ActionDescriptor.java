c232108ed04b9589f65cd7588200480ebba8955e
Maxence Bernard <maxence@maxence.info>
2007-05-01 17:48:56 +0000

    Javadoc

--- source/com/mucommander/ui/action/ActionDescriptor.java
+++ source/com/mucommander/ui/action/ActionDescriptor.java
@@ -3,40 +3,81 @@ package com.mucommander.ui.action;
 import java.util.Hashtable;
 
 /**
- * 
+ * A descriptor class for {@link MucoAction} instances. An ActionDescriptor is the combination of a MucoAction
+ * class (a class extending MucoAction and following its conventions) and a set of properties used for instanciation.
+ * Thus, it not only identifies an action class but also the way it is instanciated.
+ *
+ * <p>Two ActionDescriptor instances are equal only if:
+ * <ul>
+ *  <li>they refer to the same MucoAction class
+     *  <li>both sets of initialization properties are equal, i.e. they contain the same key/value pairs (deep equality)
+ * </ul>
+ * This means that two ActionDescriptor instances referring to the same MucoAction class but with a different set of
+ * initialization properties will not be equal.
+ *
+ * <p>This class is used by ActionManager to instance MucoAction and allow several instances to live in memory only if
+ * they have different initialization properties, which translated into action speak means a different appearance
+ * and/or behavior.
+ *
+ * @see ActionManager, MucoAction
  *
  * @author Maxence Bernard
  */
 public class ActionDescriptor {
 
+    /** MucoAction class descriptor */
     private Class actionClass;
+
+    /** */
     private Hashtable properties;
 
 
+    /**
+     * Convenience constructor which has the same effect as calling {@link #ActionDescriptor(Class, Hashtable)}
+     * with a null value for the <code>properties</code> parameter.
+     *
+     * @param actionClass a MucoAction Class descriptor
+     */
     public ActionDescriptor(Class actionClass) {
         this(actionClass, null);
     }
 
-
+    /**
+     * Creates a new ActionDescriptor which identifies the specified combination of MucoAction class and initialization
+     * properties. The <code>properties</code> parameter may be <code>null</code> if the action class is to be
+     * instanciated without any initialization properties.
+     *
+     * <p>The specified Class *must* denote a class that extends {@link MucoAction} and follows its conventions
+     * (provide the proper constructor), otherwise {@link ActionManager} will fail to instanciate it. However,
+     * ActionDescriptor does not check if the specified Class is valid or not.
+     * 
+     * @param actionClass a MucoAction Class descriptor
+     * @param properties a Hashtable containing the properties that will be used to instanciate the specified MucoAction class
+     */
     public ActionDescriptor(Class actionClass, Hashtable properties) {
         this.actionClass = actionClass;
         this.properties = properties;
     }
 
-
+    /**
+     * Returns a Class instance referring to a class that extends MucoAction.
+     */
     public Class getActionClass() {
         return actionClass;
     }
 
-
+    /**
+     * Returns the list of properties that are used to instanciate the MucoAction class.
+     */
     public Hashtable getActionProperties() {
         return properties;
     }
 
 
     /**
-     * Returns true if the given object is an ActionDescriptior that is equal to this one. ActionDescriptor instances
-     * are considered as equal if they have the same class and properties.
+     * Returns <code>true</code> if the given object is an ActionDescriptor that is equal to this one.
+     * ActionDescriptor instances are considered equal if they refer to the same {@link MucoAction} class and
+     * set of initialization properties.
      */
     public boolean equals(Object o) {
         if(!(o instanceof ActionDescriptor))
@@ -51,12 +92,15 @@ public class ActionDescriptor {
 
 
     /**
-     * Returns a hash code value for this ActionDescriptor, making this class suitable for use as a key in a Map.
+     * Returns a hash code value for this ActionDescriptor, making this class suitable for use as a key in a Hashtable.
      */
     public int hashCode() {
         return actionClass.hashCode() + 27*(properties==null?0:properties.hashCode());
     }
 
+    /**
+     * Returns a String representation of this ActionDescriptor. 
+     */
     public String toString() {
         return super.toString()+" class="+actionClass.getName()+" properties="+properties;
     }
