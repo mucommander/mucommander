63774cc057671edea407936a163c7156e3092d1c
Maxence Bernard <maxence@maxence.info>
2007-07-02 23:10:10 +0000

    Added missing GPL headers. Need to automate this or I will soon go nuts.

--- source/com/mucommander/file/impl/trash/QueuedTrash.java
+++ source/com/mucommander/file/impl/trash/QueuedTrash.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ * Copyright (C) 2002-2007 Maxence Bernard
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mucommander.file.impl.trash;
 
 import com.mucommander.file.AbstractFile;
