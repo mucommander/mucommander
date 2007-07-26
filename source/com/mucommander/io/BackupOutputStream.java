94e7f956cce0672bd63c9f8c6e15c8ddc84131be
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-07-26 13:18:21 +0000

    Can (and should) now be used as a normal output stream, rather than using direct calls to close(boolean).

--- source/com/mucommander/io/BackupOutputStream.java
+++ source/com/mucommander/io/BackupOutputStream.java
@@ -49,6 +49,11 @@ import java.io.*;
  * Files that have been saved by this class should be read with {@link com.mucommander.io.BackupInputStream}
  * in order to make sure that an uncorrupt version of them is loaded.
  * </p>
+ * <p>
+ * The <code>BackupOutputStream</code> monitors all of its own I/O operations. If an error occurs, then the backup
+ * operation will not be performed when {@link #close()} is called. It's possible to force the backup operation by
+ * using the {@link #close(boolean)} method.
+ * </p>
  * @see    com.mucommander.io.BackupInputStream
  * @author Nicolas Rinaudo
  */
@@ -59,6 +64,8 @@ public class BackupOutputStream extends FilterOutputStream implements BackupCons
     private AbstractFile     target;
     /** Path to the backup file. */
     private AbstractFile     backup;
+    /** Whether or not an error occured while writing to the backup file. */
+    private boolean          error;
 
 
 
@@ -99,57 +106,126 @@ public class BackupOutputStream extends FilterOutputStream implements BackupCons
 
 
 
+    // - Error catching ---------------------------------------------------------
+    // --------------------------------------------------------------------------
+    /**
+     * Flushes this output stream and forces any buffered output bytes to be written out to the stream.
+     * <p>
+     * This method calls the <code>flush()</code> method of its underlying output stream.
+     * </p>
+     * <p>
+     * If an error occurs at this point, the {@link #close()} method will not overwrite the target file. This can be
+     * forced through the {@link #close(boolean)} method.
+     * </p>
+     * @throws IOException if an I/O error occurs.
+     */
+    public void flush() throws IOException {
+        if(error)
+            super.flush();
+        else {
+            try {super.flush();}
+            catch(IOException e) {
+                error = true;
+                throw e;
+            }
+        }
+    }
+
+    /**
+     * Writes b.length bytes to this output stream.
+     * <p>
+     * This method calls the <code>write(byte[] b)</code> method of its underlying output stream.
+     * </p>
+     * <p>
+     * If an error occurs at this point, the {@link #close()} method will not overwrite the target file. This can be
+     * forced through the {@link #close(boolean)} method.
+     * </p>
+     * @param  b           the data to be written.
+     * @throws IOException if an I/O error occurs.
+     */
+    public void write(byte[] b) throws IOException {
+        if(error)
+            super.write(b);
+        else {
+            try {super.write(b);}
+            catch(IOException e) {
+                error = true;
+                throw e;
+            }
+        }
+    }
+
+    /**
+     * Writes len bytes from the specified byte array starting at offset off to this output stream.
+     * <p>
+     * This method calls the <code>write(byte[] b, int off, int len)</code> method of its underlying output stream.
+     * </p>
+     * <p>
+     * If an error occurs at this point, the {@link #close()} method will not overwrite the target file. This can be
+     * forced through the {@link #close(boolean)} method.
+     * </p>
+     * @param  b           the data to be written.
+     * @param  off         the start offset in the data.
+     * @param  len         the number of bytes to write.
+     * @throws IOException if an I/O error occurs.
+     */
+    public void write(byte[] b, int off, int len) throws IOException {
+        if(error)
+            super.write(b, off, len);
+        else {
+            try {super.write(b, off, len);}
+            catch(IOException e) {
+                error = true;
+                throw e;
+            }
+        }
+    }
+
+    /**
+     * Writes the specified byte to this output stream.
+     * <p>
+     * This method calls the <code>write(byte b)</code> method of its underlying output stream.
+     * </p>
+     * <p>
+     * If an error occurs at this point, the {@link #close()} method will not overwrite the target file. This can be
+     * forced through the {@link #close(boolean)} method.
+     * </p>
+     * @param  b           the data to be written.
+     * @throws IOException if an I/O error occurs.
+     */
+    public void write(int b) throws IOException {
+        if(error)
+            super.write(b);
+        else {
+            try {super.write(b);}
+            catch(IOException e) {
+                error = true;
+                throw e;
+            }
+        }
+    }
+
+
+
     // - Backup -----------------------------------------------------------------
     // --------------------------------------------------------------------------
     /**
-     * Overwrites the target file with the backup one>
+     * Overwrites the target file with the backup one.
      * @exception IOException thrown if any IO related error occurs.
      */
     private void backup() throws IOException {
-        InputStream  in;     // Input stream on the backup file.
-        OutputStream out;   // Output stream on the target file.
-        byte[]       buffer; // Stores chunks of the backup file.
-        int          count;  // Number of bytes read in the last read operation.
-
-        in  = null;
-        out = null;
-
-        try {
-            // Initialises transfer.
-            in     = backup.getInputStream();
-            out    = target.getOutputStream(false);
-            buffer = new byte[512];
-
-            // Transfers the content of in into out.
-            while((count = in.read(buffer)) != -1)
-                out.write(buffer, 0, count);
-
-            // Frees the input stream in order to delete the backup file.
-            in.close();
-            in = null;
-
-            // Deletes the backup file.
-            backup.delete();
-        }
-        // Cleanup.
-        finally {
-            if(in != null) {
-                try {in.close();}
-                catch(Exception e) {}
-            }
-
-            if(out != null) {
-                try {out.close();}
-                catch(Exception e) {}
-            }
-        }
+        // We're not using backup.moveTo(target) because we want to make absolutely sure
+        // that if an error occurs in the middle of the operation, at least one of the two files
+        // is complete.
+        backup.copyTo(target);
+        backup.delete();
     }
 
     /**
      * Finishes the backup operation.
      * @exception IOException thrown if any IO related error occurs.
      */
-    public void close() throws IOException {close(true);}
+    public void close() throws IOException {close(!error);}
 
     /**
      * Closes the output stream.
