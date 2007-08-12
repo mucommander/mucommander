67e0211ba4a6f30e2fc56caec558ea3990a9fc85
Maxence Bernard <maxence@maxence.info>
2007-08-12 20:03:19 +0000

    Initial revision.

---
+++ source/com/mucommander/io/BufferedRandomOutputStream.java
@@ -0,0 +1,207 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ * Copyright (C) 2002-2007 Maxence Bernard
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mucommander.io;
+
+import java.io.IOException;
+
+/**
+ * BufferedRandomOutputStream is a buffered output stream for {@link RandomAccessOutputStream} which, unlike
+ * <code>java.io.BufferedOutputStream</code>, makes it safe to seek in the RandomAccessOutputStream.
+ *
+ * <p>This class uses {@link BufferPool} to create the internal buffer, to avoid excessive memory allocation and
+ * garbage collection.
+ *
+ * @author Maxence Bernard
+ */
+public class BufferedRandomOutputStream extends RandomAccessOutputStream {
+
+    /** The underlying random access output stream */
+    private RandomAccessOutputStream raos;
+
+    /** The buffer where written bytes are accumulated before being sent to the underlying output stream */
+    private byte buffer[];
+
+    /** The current number of bytes waiting to be flushed to the underlying output stream */
+    private int count;
+
+    /** The default buffer size if none is specified */
+    public final static int DEFAULT_BUFFER_SIZE = 65536;
+
+
+    /**
+     * Creates a new <code>BufferedRandomOutputStream</code> on top of the given
+     * <code>RandomAccessOutputStream</code>. An internal buffer of {@link #DEFAULT_BUFFER_SIZE} bytes is created.
+     *
+     * @param raos the underlying RandomAccessOutputStream used by this buffered output stream
+     */
+    public BufferedRandomOutputStream(RandomAccessOutputStream raos) {
+        this(raos, DEFAULT_BUFFER_SIZE);
+    }
+
+    /**
+     * Creates a new <code>BufferedRandomOutputStream</code> on top of the given
+     * <code>RandomAccessOutputStream</code>. An internal buffer of the given size is created.
+     *
+     * @param raos the underlying RandomAccessOutputStream used by this buffered output stream
+     * @param size size of the buffer in bytes
+     */
+    public BufferedRandomOutputStream(RandomAccessOutputStream raos, int size) {
+        this.raos = raos;
+        this.buffer = BufferPool.getBuffer(size);
+    }
+
+    /**
+     * Flushes the internal buffer.
+     *
+     * @throws IOException if an error occurs
+     */
+    private void flushBuffer() throws IOException {
+        if (count > 0) {
+            raos.write(buffer, 0, count);
+            count = 0;
+        }
+    }
+
+
+    /////////////////////////////////////////////
+    // RandomAccessOutputStream implementation //
+    /////////////////////////////////////////////
+
+    /**
+     * Writes the specified byte to this buffered output stream.
+     *
+     * @param b the byte to be written
+     * @throws IOException if an I/O error occurs
+     */
+    public synchronized void write(int b) throws IOException {
+        if (count >= buffer.length)
+            flushBuffer();
+
+        buffer[count++] = (byte)b;
+    }
+
+    /**
+     * Writes the specified byte array to this buffered output stream.
+     *
+     * @param b the bytes to be written
+     * @throws IOException if an I/O error occurs
+     */
+    public synchronized void write(byte b[]) throws IOException {
+        write(b, 0, b.length);
+    }
+
+    /**
+     * Writes <code>len</code> bytes from the specified byte array starting at offset <code>off</code> to this
+     * buffered output stream.
+     *
+     * <p>Usually this method stores bytes from the given array into this
+     * stream's buffer, flushing the buffer to the underlying output stream as
+     * needed. However, if the requested data length is equal or larger than this stream's
+     * buffer, then this method will flush the buffer and write the
+     * bytes directly to the underlying output stream. Thus redundant
+     * <code>RandomBufferedOutputStream</code>s will not copy data unnecessarily.
+     *
+     * @param b the data.
+     * @param off the start offset in the data.
+     * @param len the number of bytes to write.
+     * @throws IOException if an I/O error occurs.
+     */
+    public synchronized void write(byte b[], int off, int len) throws IOException {
+        if (len >= buffer.length) {
+            /* If the request length exceeds the size of the output buffer,
+            flush the output buffer and then write the data directly.
+            In this way buffered streams will cascade harmlessly. */
+            flushBuffer();
+            raos.write(b, off, len);
+            return;
+        }
+
+        if (len > buffer.length - count)
+            flushBuffer();
+
+        System.arraycopy(b, off, buffer, count, len);
+        count += len;
+    }
+
+    /**
+     * Flushes this buffered output stream. This forces any buffered
+     * output bytes to be written out to the underlying output stream.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    public synchronized void flush() throws IOException {
+        flushBuffer();
+        raos.flush();
+    }
+
+    public synchronized long getOffset() throws IOException {
+        // Add the buffered byte count
+        return raos.getOffset() + count;
+    }
+
+    public synchronized void seek(long offset) throws IOException {
+        // Flush any buffered bytes before seeking, otherwise buffered bytes would be written at the wrong offset
+        flush();
+
+        raos.seek(offset);
+    }
+
+    public synchronized long getLength() throws IOException {
+        // Anticipate if the file is to be expanded by the bytes awaiting in the buffer
+        return Math.max(raos.getLength(), getOffset());
+    }
+
+    public synchronized void setLength(long newLength) throws IOException {
+        // Flush before changing the file's length, otherwise the behavior of setLength() would be modified, especially
+        // when truncating the file
+        flush();
+
+        raos.setLength(newLength);
+    }
+
+    public synchronized void close() throws IOException {
+        if(buffer!=null) {      // buffer is null if close() was already called
+            try {
+                flush();
+            }
+            catch(IOException e) {
+                // Continue anyway
+            }
+
+            // Release the buffer
+            BufferPool.releaseBuffer(buffer);
+            buffer = null;
+        }
+
+        raos.close();
+    }
+
+
+    ////////////////////////
+    // Overridden methods //
+    ////////////////////////
+
+    protected void finalize() throws Throwable {
+        // If this stream hasn't been closed, release the buffer before finalizing the object
+        if(buffer!=null)
+            BufferPool.releaseBuffer(buffer);
+
+        super.finalize();
+    }
+}
---
+++ source/com/mucommander/io/FilteredRandomOutputStream.java
@@ -0,0 +1,94 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ * Copyright (C) 2002-2007 Maxence Bernard
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mucommander.io;
+
+import java.io.IOException;
+
+/**
+ * FilteredRandomOutputStream is a filtered output stream for {@link RandomAccessOutputStream} subclasses, allowing
+ * to easily extend the functionality provided by the stream by overriding only a few methods.
+ * In a similar way to <code>java.io.FilteredOutputStream</code>, this class delegates all method calls to the
+ * specified <code>RandomAccessOutputStream</code>.
+ *
+ * @author Maxence Bernard
+ */
+public class FilteredRandomOutputStream extends RandomAccessOutputStream {
+
+    /** The underlying RandomAccessOutputStream */
+    protected RandomAccessOutputStream raos;
+
+    
+    /**
+     * Creates a new FilteredRandomOutputStream that delegates all method calls to the specified
+     * <code>RandomAccessOutputStream</code>.
+     *
+     * @param raos the RandomAccessOutputStream to delegate method calls to
+     */
+    public FilteredRandomOutputStream(RandomAccessOutputStream raos) {
+        this.raos = raos;
+    }
+
+
+    /////////////////////////////////////////////
+    // RandomAccessOutputStream implementation //
+    /////////////////////////////////////////////
+    
+    public void write(int b) throws IOException {
+        raos.write(b);
+    }
+
+    public void write(byte b[]) throws IOException {
+        raos.write(b);
+    }
+
+    public void write(byte b[], int off, int len) throws IOException {
+        raos.write(b, off, len);
+    }
+
+    public void setLength(long newLength) throws IOException {
+        raos.setLength(newLength);
+    }
+
+    public long getOffset() throws IOException {
+        return raos.getOffset();
+    }
+
+    public long getLength() throws IOException {
+        return raos.getLength();
+    }
+
+    public void seek(long offset) throws IOException {
+        raos.seek(offset);
+    }
+
+    public void flush() throws IOException {
+        raos.flush();
+    }
+
+    public void close() throws IOException {
+        try {
+            flush();
+        }
+        catch(IOException e) {
+            // Try closing the stream anyway
+        }
+
+        raos.close();
+    }
+}
