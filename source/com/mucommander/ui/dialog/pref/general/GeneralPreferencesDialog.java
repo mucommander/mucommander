f3d2b46c5eeb0e742e4d2302dd2f26d20b96a2e2
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-09-07 12:06:08 +0000

    Now uses a factory method to create instance rather than a public constructor. This is meant to ensure that we cannot have more than one instance of the dialog at any given time, preventing embarassing occurences of 20 preferences being opened when a user is a bit to enthusiastic in pressing the apple-, shortcut.

--- source/com/mucommander/ui/dialog/pref/general/GeneralPreferencesDialog.java
+++ source/com/mucommander/ui/dialog/pref/general/GeneralPreferencesDialog.java
@@ -21,47 +21,96 @@ package com.mucommander.ui.dialog.pref.general;
 import com.mucommander.conf.impl.MuConfiguration;
 import com.mucommander.text.Translator;
 import com.mucommander.ui.dialog.pref.PreferencesDialog;
+import com.mucommander.ui.main.WindowManager;
 
 import java.awt.*;
-
 
 /**
  * This is the main preferences dialog that contains all preferences panels organized by tabs.
  * @author Maxence Bernard, Nicolas Rinaudo
  */
 public class GeneralPreferencesDialog extends PreferencesDialog {
+    // - Singleton --------------------------------------------------------------
+    // --------------------------------------------------------------------------
+    /** Used to ensure we only have the one preferences dialog open at any given time. */
+    private static       GeneralPreferencesDialog singleton;
+    /** Used to synchronize calls to the singleton. */
+    private static final Object                   singletonLock = new Object();
+
+
+
+    // - Dimensions -------------------------------------------------------------
+    // --------------------------------------------------------------------------
+    /** Dialog's minimum dimensions. */
     private final static Dimension MINIMUM_DIALOG_DIMENSION = new Dimension(580,0);
+    /** Dialog's maximum dimensions. */
     private final static Dimension MAXIMUM_DIALOG_DIMENSION = new Dimension(640,480);
 
+
+
+    // - Available tabs ---------------------------------------------------------
+    // --------------------------------------------------------------------------
+    /** Identifier of the 'general' tab. */
     public static final int GENERAL_TAB    = 0;
+    /** Identifier of the 'folders' tab. */
     public static final int FOLDERS_TAB    = 1;
+    /** Identifier of the 'appearance' tab. */
     public static final int APPEARANCE_TAB = 2;
+    /** Identifier of the 'mail' tab. */
     public static final int MAIL_TAB       = 3;
+    /** Identifier of the 'misc' tab. */
     public static final int MISC_TAB       = 4;
 
-    // Tab icons
+
+
+    // - Tab icons --------------------------------------------------------------
+    // --------------------------------------------------------------------------
+    /** Name of the icon used by the 'general' tab. */
     private final static String GENERAL_ICON    = "general.png";
+    /** Name of the icon used by the 'folders' tab. */
     private final static String FOLDERS_ICON    = "folders.png";
-    private final static String THEMES_ICON     = "themes.png";
+    /** Name of the icon used by the 'appearance' tab. */
     private final static String APPEARANCE_ICON = "appearance.png";
+    /** Name of the icon used by the 'mail' tab. */
     private final static String MAIL_ICON       = "mail.png";
+    /** Name of the icon used by the 'misc' tab. */
     private final static String MISC_ICON       = "misc.png";
 
+
+
+    // - Misc .fields -----------------------------------------------------------
+    // --------------------------------------------------------------------------
     /** Index of the tab that was last selected by the user */
     private static int lastTabIndex = 0;
+    /** Whether or not the dialog should take tab selection events into account. */
     private boolean listenToChanges;
 
-    public GeneralPreferencesDialog(Frame parent) {
-        super(parent, Translator.get("prefs_dialog.title"));
-        initUI();
+
+
+    // - Initialisation ---------------------------------------------------------
+    // --------------------------------------------------------------------------
+    /**
+     * Returns an instance of <code>GeneralPreferencesDialog</code>.
+     * <p>
+     * This will not necessarily create a new instance - if a dialog is already in use, it
+     * will be returned. This is an attempt to ensure that the preferences dialog is not opened
+     * more than once.
+     * </p>
+     * @return an instance of <code>GeneralPreferencesDialog</code>.
+     */
+    public static GeneralPreferencesDialog getDialog() {
+        synchronized(singletonLock) {
+            if(singleton == null)
+                singleton = new GeneralPreferencesDialog();
+            return singleton;
+        }
     }
 
-    public GeneralPreferencesDialog(Dialog parent) {
-        super(parent, Translator.get("prefs_dialog.title"));
-        initUI();
-    }
-
-    private void initUI() {
+    /**
+     * Creates a new instance of the <code>GeneralPreferencesDialog</code>.
+     */
+    private GeneralPreferencesDialog() {
+        super(WindowManager.getCurrentMainFrame(), Translator.get("prefs_dialog.title"));
         listenToChanges = false;
 
         // Adds the preference tabs.
@@ -80,15 +129,30 @@ public class GeneralPreferencesDialog extends PreferencesDialog {
         listenToChanges = true;
     }
 
+
+
+    // - Misc. ------------------------------------------------------------------
+    // --------------------------------------------------------------------------
+    /**
+     * Commits the changes and writes the configuration file if necessary.
+     */
     public void commit() {
         super.commit();
         try {MuConfiguration.write();}
-        // We should probably pop an error dialog here.
+        // TODO: this should pop an error dialog.
         catch(Exception e) {}
     }
 
+    /**
+     * Listens to tab selection changes in order to store the last tab selected by the user.
+     */
     public void tabSelectionChanged(int newIndex) {
         if(listenToChanges)
             lastTabIndex = newIndex;
     }
+
+    /**
+     * Releases the singleton.
+     */
+    public void finalize() {synchronized(singletonLock) {singleton = null;}}
 }
