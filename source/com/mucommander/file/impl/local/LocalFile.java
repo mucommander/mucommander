243a8dddcc0201f99e58ae2618aa347791ff7c38
Maxence Bernard <maxence@maxence.info>
2007-10-29 15:40:39 +0000

    Modified #moveTo() to comply with the new method contract

--- source/com/mucommander/file/impl/local/LocalFile.java
+++ source/com/mucommander/file/impl/local/LocalFile.java
@@ -600,6 +600,11 @@ public class LocalFile extends AbstractFile {
             return super.moveTo(destFile);
         }
 
+        // Fail in some situations where java.io.File#renameTo() doesn't, such as if the destination already exists.
+        // Note that java.io.File#renameTo()'s implementation is system-dependant, so it's always a good idea to
+        // perform all those checks even if some are not necessary on this or that platform.
+        checkCopyPrerequisites(destFile);
+
         // Move file
         return file.renameTo(((LocalFile)destFile).file);
     }
