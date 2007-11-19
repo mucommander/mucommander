67e2ef1cfb62e75a227192081915a3b04426b8f0
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-11-19 10:43:08 +0000

    Factorises the EditAction and ViewAction code.

---
+++ source/com/mucommander/ui/action/AbstractViewerAction.java
@@ -0,0 +1,116 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ * Copyright (C) 2002-2007 Maxence Bernard
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mucommander.ui.action;
+
+import com.mucommander.command.Command;
+import com.mucommander.conf.ConfigurationEvent;
+import com.mucommander.conf.ConfigurationListener;
+import com.mucommander.conf.impl.MuConfiguration;
+import com.mucommander.file.AbstractFile;
+import com.mucommander.file.impl.local.LocalFile;
+import com.mucommander.file.FileProtocols;
+import com.mucommander.file.util.ResourceLoader;
+import com.mucommander.job.TempOpenWithJob;
+import com.mucommander.process.ProcessRunner;
+import com.mucommander.text.Translator;
+import com.mucommander.ui.dialog.file.ProgressDialog;
+import com.mucommander.ui.icon.IconManager;
+import com.mucommander.ui.main.MainFrame;
+import com.mucommander.file.filter.DirectoryFileFilter;
+
+import java.util.Hashtable;
+
+/**
+ * Customisable version of {@link InternalEditAction}.
+ * @author Maxence Bernard, Nicolas Rinaudo
+ */
+abstract class AbstractViewerAction extends SelectedFileAction implements ConfigurationListener {
+    // - Instance variables --------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /** Custom command defined in the configuration. */
+    private Command customCommand;
+    /** Whether or not to use the custom command. */
+    private boolean useCustomCommand;
+
+
+
+    // - Initialisation ------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /**
+     * Creates a new <code>EditAction</code>.
+     */
+    public AbstractViewerAction(MainFrame mainFrame, Hashtable properties) {
+        super(mainFrame, properties);
+
+        // Only enable this action if currently selected file is not a directory.
+        setSelectedFileFilter(new DirectoryFileFilter());
+
+        // Listens to configuration.
+        MuConfiguration.addConfigurationListener(this);
+    }
+
+
+
+    // - Action execution ----------------------------------------------------------------
+    // -----------------------------------------------------------------------------------
+    /**
+     * Edits the currently selected file.
+     */
+    public synchronized void performAction() {
+        AbstractFile file;
+
+        file = mainFrame.getActiveTable().getSelectedFile();
+        // If the file is editable...
+        if(file != null && !(file.isDirectory() || file.isSymlink())) {
+            // If we're using a custom editor...
+            if(useCustomCommand && customCommand != null) {
+                // If it's local, run the custom editor on it.
+                if(file.getURL().getProtocol().equals(FileProtocols.FILE) && (file instanceof LocalFile)) {
+                    try {ProcessRunner.execute(customCommand.getTokens(file), file);}
+                    catch(Exception e) {}
+                }
+                // If it's distant, copies it locally before running the custom editor on it.
+                else {
+                    ProgressDialog progressDialog = new ProgressDialog(mainFrame, Translator.get("copy_dialog.copying"));
+                    TempOpenWithJob job = new TempOpenWithJob(progressDialog, mainFrame, file, customCommand);
+                    progressDialog.start(job);
+                }
+            }
+            // If we're not using a custom editor, this action behaves exactly like its parent.
+            else
+                performInternalAction(file);
+        }
+    }
+
+    public abstract void performInternalAction(AbstractFile file);
+
+
+    /**
+     * Sets the custom editor to the specified command.
+     * @param command command to use as a custom editor.
+     */
+    protected void setCustomCommand(String command) {
+        if(command == null)
+            customCommand = null;
+        else
+            customCommand = new Command(getClass().getName(), command);
+    }
+
+    protected void setUseCustomCommand(boolean use) {useCustomCommand = use;}
+}
