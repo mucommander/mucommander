dca7f7f22c1ca4dfef02c4334ab7c1783bc8e247
Maxence Bernard <maxence@maxence.info>
2007-11-28 21:09:23 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_8_1'.

---
+++ source/com/mucommander/file/filter/PermissionsFileFilter.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ * Copyright (C) 2002-2007 Maxence Bernard
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mucommander.file.filter;
+
+import com.mucommander.PlatformManager;
+import com.mucommander.file.AbstractFile;
+import com.mucommander.file.FilePermissions;
+
+/**
+ * Filter on a file's permissions.
+ * @author Nicolas Rinaudo
+ */
+public class PermissionsFileFilter extends FileFilter implements FilePermissions {
+    private int     permission;
+    private boolean filter;
+
+    public PermissionsFileFilter(int permission, boolean filter) {
+        this.permission = permission;
+        this.filter     = filter;
+    }
+
+    public boolean accept(AbstractFile file) {
+        if((permission == EXECUTE_PERMISSION) && (PlatformManager.getJavaVersion() <= PlatformManager.JAVA_1_5))
+            return true;
+
+        return filter ? file.getPermission(USER_ACCESS, permission) : !file.getPermission(USER_ACCESS, permission);
+    }
+
+    public int getPermission() {return permission;}
+    public boolean getFilter() {return filter;}
+}
+
