f95f9492274ad2b33515833305c14c1014e563e8
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-12-15 21:35:52 +0000

    Renamed the com.mucommander.bookmark.file.bookmark package to com.mucommander.bookmark.file.

--- source/com/mucommander/bookmark/file/BookmarkOutputStream.java
+++ source/com/mucommander/bookmark/file/BookmarkOutputStream.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.mucommander.bookmark.file.bookmark;
+package com.mucommander.bookmark.file;
 
 import com.mucommander.bookmark.Bookmark;
 import com.mucommander.bookmark.BookmarkBuilder;
