44662138007334c1adb5ffa755a2e76f872c72c0
Nicolas Rinaudo <nicolas@nrinaudo.com>
2007-12-28 13:08:47 +0000

    Small optimisation: the current path is now stored in a StringBuffer that is either appended to or truncated rather than creating mutliple instances of String.

--- source/com/mucommander/file/util/PathTokenizer.java
+++ source/com/mucommander/file/util/PathTokenizer.java
@@ -18,8 +18,6 @@
 
 package com.mucommander.file.util;
 
-import com.mucommander.Debug;
-
 import java.util.Enumeration;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
@@ -69,7 +67,7 @@ public class PathTokenizer implements Enumeration {
     /** Current index in the token array */
     private int currentIndex;
     /** Path part that has been tokenized */
-    private String currentPath;
+    private StringBuffer currentPath;
     /** Last separators token */ 
     private String lastSeparator;
 
@@ -119,7 +117,10 @@ public class PathTokenizer implements Enumeration {
         }
 
         // Initialize current path
-        currentPath = reverseOrder?path:"";
+        if(reverseOrder)
+            currentPath = new StringBuffer(path);
+        else
+            currentPath = new StringBuffer(path.length());
 
         // Skip leading separator
         skipSeparators();
@@ -139,10 +140,7 @@ public class PathTokenizer implements Enumeration {
             lastSeparator += token;
 
             // Update current path
-            if(reverseOrder)
-                currentPath = currentPath.substring(0, currentPath.length()-token.length());
-            else
-                currentPath += token;
+            handleToken(token);
 
             currentIndex++;
         }
@@ -156,6 +154,12 @@ public class PathTokenizer implements Enumeration {
         return currentIndex<tokens.length;
     }
 
+    private void handleToken(String token) {
+        if(reverseOrder)
+            currentPath.setLength(currentPath.length()-token.length());
+        else
+            currentPath.append(token);
+    }
 
     /**
      * Returns the next filename token from this PathTokenizer. Throws a NoSuchElementException if no more filename
@@ -170,10 +174,7 @@ public class PathTokenizer implements Enumeration {
             String token = tokens[currentIndex++];
 
             // Update current path
-            if(reverseOrder)
-                currentPath = currentPath.substring(0, currentPath.length()-token.length());
-            else
-                currentPath += token;
+            handleToken(token);
 
             // Skip separators after the filename 
             skipSeparators();
@@ -192,7 +193,7 @@ public class PathTokenizer implements Enumeration {
      * tokenized.
      */
     public String getCurrentPath() {
-        return currentPath;
+        return currentPath.toString();
     }
 
 
@@ -226,61 +227,5 @@ public class PathTokenizer implements Enumeration {
      */
     public Object nextElement() throws NoSuchElementException {
         return nextFilename();
-    }
-
-
-    //////////////////
-    // Test methods //
-    //////////////////
-
-    public static void main(String args[]) {
-        test("/Users/maxence/Temp");
-        test("/Users/maxence/Temp/");
-        test("/");
-        test("C:\\temp");
-        test("C:\\temp\\");
-        test("C:\\");
-        test("C:");
-        test("/C:\\temp");
-        test("/C:\\\\this///is/not\\\\a\\valid//path//but/we\\let//it\\parse/");
-        test("blah");
-        test("C:");
-        test("");
-    }
-
-    private static void test(String path) {
-
-        for(boolean reverseOrder=false; ; reverseOrder=true) {
-            if(Debug.ON) Debug.trace("tokenizing= "+path+" reverseOrder="+reverseOrder);
-
-            PathTokenizer pt = new PathTokenizer(path, DEFAULT_SEPARATORS, reverseOrder);
-            
-            if(Debug.ON) Debug.trace("getLastSeparator()= "+pt.getLastSeparator());
-            if(Debug.ON) Debug.trace("getCurrentPath()= "+pt.getCurrentPath());
-            String reconstructedPath = pt.getLastSeparator();
-            if(!reverseOrder)
-                if(Debug.ON) Debug.trace("reconstructedPath= "+reconstructedPath);
-
-            while(pt.hasMoreFilenames()) {
-                String nextToken = pt.nextFilename();
-                String lastSeparator = pt.getLastSeparator();
-                if(Debug.ON) Debug.trace("nextToken()= "+nextToken);
-                if(Debug.ON) Debug.trace("getLastSeparator()= "+lastSeparator);
-                if(Debug.ON) Debug.trace("getCurrentPath()= "+pt.getCurrentPath());
-
-                if(!reverseOrder) {
-                    reconstructedPath += nextToken+lastSeparator;
-                    if(Debug.ON) Debug.trace("reconstructedPath= "+reconstructedPath);
-                }
-            }
-
-            if(!reverseOrder && !reconstructedPath.equals(path))
-                if(Debug.ON) Debug.trace("TEST FAILED");
-
-            if(Debug.ON) Debug.trace("");
-
-            if(reverseOrder)
-                break;
-        }
     }
 }
