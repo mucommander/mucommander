1145827817e1e6b3db9cc3ccf42053d8f36b4b2c
Nicolas Rinaudo <nicolas@nrinaudo.com>
2008-01-14 12:22:18 +0000

    Updated the libpath to be more flexible and allow for jsse.jar discovery in addition to rt.jar discovery.
    muCommander now tries to discover the path to jsse.jar, but this can be 'hard-coded' in the local.xml file.

--- source/com/mucommander/ant/util/SystemClasspathTask.java
+++ source/com/mucommander/ant/util/SystemClasspathTask.java
@@ -25,43 +25,98 @@ import java.io.File;
 import java.util.StringTokenizer;
 
 /**
- * Ant task meant to extract the rt.jar file from a path.
+ * Ant task meant to extract specific libraries from a given path.
  * @author Nicolas Rinaudo
  * @ant.task name="libpath" category="util"
  */
 public class SystemClasspathTask extends Task {
+    // - Instance fields -------------------------------------------------------
+    // -------------------------------------------------------------------------
+    /** Name(s) of the library we're looking for. */
+    private String library;
+    /** Path in which to look for the library. */
     private String path;
     /** Property in which to store the approved path. */
     private String antProperty;
 
+
+
+    // - Initialisation --------------------------------------------------------
+    // -------------------------------------------------------------------------
+    /**
+     * Creates a new <code>SystemClasspathTask</code> instance.
+     */
     public SystemClasspathTask() {}
 
+    /**
+     * Initialises the task.
+     */
     public void init() {
         antProperty = null;
         path        = null;
     }
 
+
+    // - Ant interaction -------------------------------------------------------
+    // -------------------------------------------------------------------------
+    /**
+     * Sets the name(s) of the library to look for.
+     * <p>
+     * Some libraries have a different name depending on the OS. For example, Sun's <code>rt.jar</code>
+     * is called <code>classes.jar</code> under MAC OS X. For these libraries, it's possible to specify
+     * more than one name by separating each name by <code>:</code>.
+     * </p>
+     * @param        s name(s) of the library to look for.
+     * @ant.required
+     */
+    public void setLibrary(String s) {library = s;}
+
+    /**
+     * Sets the name of the property in which to store the full library path.
+     * @param        s name of the property in which to store the full library path.
+     * @ant.required
+     */
     public void setProperty(String s) {antProperty = s;}
+
+    /**
+     * Sets the path in which to look for the library.
+     * <p>
+     * This can be one or more files. If more than one file is specified, each path must be separated
+     * by <code>path.separator</code>.
+     * </p>
+     * @param            s path in which to look for the library.
+     * @ant.not-required   Defaults to the value of system property <code>sun.boot.class.path</code>.
+     */
     public void setPath(String s) {path = s;}
 
+    /**
+     * Runs the task.
+     * @throws BuildException if an error occurs.
+     */
     public void execute() throws BuildException {
         StringTokenizer parser; // Used to parse the boot class path.
-        File            file;   // Current JAR file.
+        File            file;
+        String[]        names;
 
         // Makes sure that the output property has been set.
         if(antProperty == null)
-            throw new BuildException("Unspecified output property - please fill in the out attribute.");
+            throw new BuildException("Unspecified output property - please fill in the property attribute.");
         if(path == null)
             path = System.getProperty("sun.boot.class.path");
+        if(library == null)
+            throw new BuildException("Unspecified library property - please fill in the library attribute.");
 
         parser = new StringTokenizer(path, System.getProperty("path.separator"));
+        names  = library.split(":");
         while(parser.hasMoreTokens()) {
             file = new File(parser.nextToken());
-
-            // If we've found a file that exists and has a legal 'rt.jar' name, use that.
-            if(file.exists() && (file.getName().equalsIgnoreCase("rt.jar") || file.getName().equalsIgnoreCase("classes.jar"))) {
-                getProject().setNewProperty(antProperty, file.getAbsolutePath());
-                return;
+            if(file.exists()) {
+                for(int i = 0; i < names.length; i++) {
+                    if(names[i].equalsIgnoreCase(file.getName())) {
+                        getProject().setNewProperty(antProperty, file.getAbsolutePath());
+                        return;
+                    }
+                }
             }
         }
     }
