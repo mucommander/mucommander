c1acb0820b2cc4c3d4e35b8c006876d8bec5de12
Maxence Bernard <maxence@maxence.info>
2008-01-16 18:17:09 +0000

    - the code for AppleScript execution has been moved to the AppleScript class - #moveToTrash(Vector) has a specific script for AppleScript 2.0+ which has full Unicode support - fixes a bug in #getItemCount()

--- source/com/mucommander/file/impl/trash/OSXTrash.java
+++ source/com/mucommander/file/impl/trash/OSXTrash.java
@@ -22,12 +22,9 @@ import com.mucommander.Debug;
 import com.mucommander.file.AbstractFile;
 import com.mucommander.file.FileFactory;
 import com.mucommander.file.impl.local.LocalFile;
-import com.mucommander.process.AbstractProcess;
-import com.mucommander.process.ProcessRunner;
+import com.mucommander.ui.macosx.AppleScript;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.util.Vector;
 
@@ -36,20 +33,18 @@ import java.util.Vector;
  * to the trash.
  *
  * <p>
- * <b>Implementation notes:</b><br>
- * <br>
- * This trash is implemented as a {@link QueuedTrash} for several reasons:
- * <ul>
- *  <li>the Finder plays a sound when it has been told to move a file to the trash and is done with it.
- *   Moving files to the trash repeatedly would play the sound as many times as the Finder has been told to move a file,
- *   which obviously is very ugly.
- *  <li>executing an AppleScript has a cost as it has to be compiled first. When files are moved repeatedly, it is more
- *   efficient to group files and execute only one AppleScript.
- * </ul>
- * <br>
- * AppleScript is used to interact with the Finder. Scripts are executed using the 'osascript' command. Another way
- * to run scripts is to use Apple's Cocoa-Java API, but since it is deprecated, it's better to avoid using it when 
- * possible.
+ *   <b>Implementation notes:</b><br/>
+ *   <br/>
+ *   This trash is implemented as a {@link QueuedTrash} for several reasons:
+ *   <ul>
+ *    <li>the Finder plays a sound when it has been told to move a file to the trash and is done with it.
+ *        Moving files to the trash repeatedly would play the sound as many times as the Finder has been told to move a
+ *        file, which is obviously ugly.</li>
+ *    <li>executing an AppleScript has a cost as it has to be compiled first. When files are moved repeatedly, it is
+ *        more efficient to group files and execute only one AppleScript.</li>
+ *   </ul>
+ *   <br/>
+ *   This class uses {@link com.mucommander.ui.macosx.AppleScript} to interact with the trash.
  * </p>
  *
  * @author Maxence Bernard
@@ -57,21 +52,24 @@ import java.util.Vector;
 public class OSXTrash extends QueuedTrash {
 
     /** AppleScript that reveals the trash in Finder */
-    private final static String REVEAL_TRASH_APPLESCRIPT = "tell application \"Finder\" to open trash";
+    private final static String REVEAL_TRASH_APPLESCRIPT =
+        "tell application \"Finder\" to open trash\n" +
+        "activate application \"Finder\"\n";
 
     /** AppleScript that counts and returns the number of items in Trash */
-    private final static String COUNT_TRASH_ITEMS_APPLESCRIPT = "tell application \"Finder\" to return count of items in trash";
+    private final static String COUNT_TRASH_ITEMS_APPLESCRIPT =
+            "tell application \"Finder\" to return count of items in trash";
 
     /** AppleScript that empties the trash */
     private final static String EMPTY_TRASH_APPLESCRIPT = "tell application \"Finder\" to empty trash";
 
     /**
-     * AppleScript that moves files to the trash. Because AppleScript doesn't allow Unicode in the script itself
-     * (only Mac OS Roman), this script is a bit more complicated that one would like in order to support files with
-     * extended/unicode characters. The only way to deal with UTF-8 strings in AppleScript is to read them from a file.
-     * See http://www.satimage.fr/software/en/unicode_and_applescript.html for more info about Unicode and AppleScript.
+     * AppleScript that moves files to the trash, for versions of AppleScript (1.10 or lower )that do not allow Unicode
+     * in the script itself (only MacRoman). As a result, this script is more complicated as the only way to deal with
+     * Unicode text is to read them from a file. See http://www.satimage.fr/software/en/unicode_and_applescript.html
+     * for more info about this workaround.
      */
-    private final static String MOVE_TO_TRASH_APPLESCRIPT =
+    private final static String MOVE_TO_TRASH_APPLESCRIPT_NO_UNICODE =
         // Loads the contents of the UTF8-encoded file which path is contained in the 'tmpFilePath' variable.
         // This variable must be set before the beginning of the script. This file contains the list of files to move
         // to the trash, separated by EOL characters. The file must NOT end with a trailing EOL.
@@ -93,178 +91,6 @@ public class OSXTrash extends QueuedTrash {
         "tell application \"Finder\" to move fileList to the trash";
 
 
-//    private static boolean isAvailable;
-
-//    static {
-//        // Test if the Cocoa-java library is available
-//        if(PlatformManager.OS_FAMILY==PlatformManager.MAC_OS_X) {
-//            try {
-//                Class.forName("com.apple.cocoa.foundation.NSAppleScript");
-//                // Seems OK
-//                isAvailable = true;
-//            }
-//            catch(Exception e) {
-//                // Not available
-//            }
-//        }
-//
-//        if(Debug.ON) Debug.trace("isAvailable="+isAvailable);
-//    }
-
-//    /**
-//     * Returns <code>true</code> if this trash can be instanciated, <code>false</code> if the current OS is not
-//     * Mac OS X or if the Cocoa-java library is not available.
-//     *
-//     * @return true if this trash can be instanciated, false if the current OS is not Mac OS X or if the Cocoa-java
-//     * library is not available
-//     */
-//    public static boolean isAvailable() {
-//        return isAvailable;
-//    }
-
-
-    // The following commented method executes an AppleScript using the Cocoa-Java API.
-    // We're now using the 'osascript' command instead, but this method is kept for the record.
-
-//    /**
-//     * Executes the given AppleScript and returns the script's output if it was successfully executed, <code>null</code>
-//     * if the script couldn't be compiled or if an error occurred while executing it.
-//     * An empty string <code>""</code> is returned if the script doesn't output anything.
-//     *
-//     * @param appleScript the AppleScript to compile and execute
-//     * @return the script's output, null if an error occurred while compiling or executing the script
-//     */
-//    private static String executeAppleScript(String appleScript) {
-//        if(Debug.ON) Debug.trace("Executing AppleScript "+appleScript);
-//
-//        int pool = -1;
-//
-//        try {
-//            // Quote from Apple Cocoa-Java doc:
-//            // An autorelease pool is used to manage Foundation’s autorelease mechanism for Objective-C objects.
-//            // NSAutoreleasePool provides Java applications access to autorelease pools. Typically it is not
-//            // necessary for Java applications to use NSAutoreleasePools since Java manages garbage collection.
-//            // However, some situations require an autorelease pool; for instance, if you start off a thread that
-//            // calls Cocoa, there won’t be a top-level pool.
-//            pool = NSAutoreleasePool.push();
-//
-//            NSMutableDictionary errorInfo = new NSMutableDictionary();
-//            NSAppleEventDescriptor eventDescriptor = new NSAppleScript(appleScript).execute(errorInfo);
-//            if(eventDescriptor==null) {
-//                if(Debug.ON)
-//                    Debug.trace("Caught AppleScript error: "+errorInfo.objectForKey(NSAppleScript.AppleScriptErrorMessage));
-//
-//                return null;
-//            }
-//
-//            String output = eventDescriptor.stringValue();  // Returns null if the script didn't output anything
-//            if(Debug.ON) Debug.trace("AppleScript output="+output);
-//
-//            return output==null?"":output;
-//        }
-//        catch(Error e) {
-//            // Can happen if Cocoa-java is not in the classpath
-//            if(Debug.ON) Debug.trace("Unexcepted error while executing AppleScript (cocoa-java not available?): "+e);
-//
-//            return null;
-//        }
-//        catch(Exception e) {
-//            // Try block is not supposed to throw any exception, but this is low-level stuff so just to be safe
-//            if(Debug.ON) Debug.trace("Unexcepted exception while executing AppleScript: "+e);
-//
-//            return null;
-//        }
-//        finally {
-//            if(pool!=-1)
-//                NSAutoreleasePool.pop(pool);
-//        }
-//    }
-
-
-    /**
-     * Executes the given AppleScript and returns <code>true</code> if it was successfully executed.
-     * The script's output is appended to the given <code>StringBuffer</code>. If the caller is not interested in the
-     * script's output, a <code>null</code> value can be passed.
-     *
-     * @param appleScript the AppleScript to execute
-     * @param output the StringBuffer that will hold the script's output, <code>null</code> for no output
-     * @return true if the script was succesfully executed
-     */
-    private static boolean executeAppleScript(String appleScript, StringBuffer output) {
-        if(Debug.ON) Debug.trace("Executing AppleScript "+appleScript);
-
-        // Use the 'osascript' command to execute the AppleScript. The '-s o' flag tells osascript to print errors to
-        // stdout rather than stderr. The AppleScript is piped to the process instead of passing it as an argument
-        // ('-e' flag), for better control over the encoding and to remove any limitations on the maximum script size.
-        String tokens[] = new String[] {
-            "osascript",
-            "-s",
-            "o",
-        };
-
-        OutputStreamWriter pout = null;
-        InputStream pin = null;
-        try {
-            // Execute the osascript command.
-            AbstractProcess process = ProcessRunner.execute(tokens);
-
-            if(output!=null)
-                pin = process.getInputStream();
-
-            // Pipe the script to the osascript process.
-            // For some strange reason, osascript expects MacRoman encoding, not UTF-8 as one would have thought
-            pout = new OutputStreamWriter(process.getOutputStream(), "MacRoman");
-            pout.write(appleScript);
-            pout.close();
-
-            if(output!=null) {
-                // Append the script's output to the given StringBuffer
-                byte buffer[] = new byte[128];
-                int nbRead;
-                ByteArrayOutputStream bout = new ByteArrayOutputStream();
-                while((nbRead=pin.read(buffer))!=-1)
-                    bout.write(buffer, 0, nbRead);
-
-                output.append(bout.toString("utf-8"));
-            }
-
-            int returnCode = process.waitFor();
-
-            if(Debug.ON) Debug.trace("osascript returned "+returnCode+" output="+output);
-
-            if(returnCode!=0) {
-                if(Debug.ON) Debug.trace("osascript terminated abnormally");
-                return false;
-            }
-
-            return true;
-        }
-        catch(Exception e) {        // IOException, InterruptedException
-            // Shouldn't normally happen
-            if(Debug.ON) {
-                Debug.trace("Unexcepted exception while executing AppleScript: "+e);
-                e.printStackTrace();
-            }
-
-            try {
-                if(pout!=null)
-                    pout.close();
-            }
-            catch(IOException e1) {
-            }
-
-            try {
-                if(pin!=null)
-                    pin.close();
-            }
-            catch(IOException e2) {
-            }
-
-            return false;
-        }
-    }
-
-
     //////////////////////////////////
     // AbstractTrash implementation //
     //////////////////////////////////
@@ -284,7 +110,7 @@ public class OSXTrash extends QueuedTrash {
     }
 
     public boolean empty() {
-        return executeAppleScript(EMPTY_TRASH_APPLESCRIPT, null);
+        return AppleScript.execute(EMPTY_TRASH_APPLESCRIPT, null);
     }
 
     public boolean isTrashFile(AbstractFile file) {
@@ -294,7 +120,7 @@ public class OSXTrash extends QueuedTrash {
 
     public int getItemCount() {
         StringBuffer output = new StringBuffer();
-        if(!executeAppleScript(COUNT_TRASH_ITEMS_APPLESCRIPT, output))
+        if(!AppleScript.execute(COUNT_TRASH_ITEMS_APPLESCRIPT, output))
             return -1;
 
         try {
@@ -307,7 +133,7 @@ public class OSXTrash extends QueuedTrash {
     }
 
     public void open() {
-        executeAppleScript(REVEAL_TRASH_APPLESCRIPT, null);
+        AppleScript.execute(REVEAL_TRASH_APPLESCRIPT, null);
     }
 
     /**
@@ -340,66 +166,73 @@ public class OSXTrash extends QueuedTrash {
      * </ul>
      */
     protected boolean moveToTrash(Vector queuedFiles) {
+        String appleScript;
 
-        // Simple AppleScript that moves the specified file paths to the trash. This script does not support Unicode
-        // paths, this is why we don't use it (kept for the record)
-
-//        int nbFiles = queuedFiles.size();
-//        String appleScript = "tell application \"Finder\" to move {";
-//        for(int i=0; i<nbFiles; i++) {
-//            appleScript += "posix file \""+((AbstractFile)queuedFiles.elementAt(i)).getAbsolutePath()+"\"";
-//            if(i<nbFiles-1)
-//                appleScript += ", ";
-//        }
-//        appleScript += "} to the trash";
-
-        AbstractFile tmpFile = null;
-        OutputStreamWriter tmpOut = null;
-
-        try {
-            // Create the temporary file that contains the list of files to move, encoded as UTF-8 and separated by
-            // EOL characters. The file must NOT end with a trailing EOL.
+        // Simple script for AppleScript versions with Unicode support, i.e. that allows Unicode characters in the
+        // script (AppleScript 2.0 / Mac OS X 10.5 or higher).
+        if(AppleScript.getScriptEncoding().equals(AppleScript.UTF8)) {
             int nbFiles = queuedFiles.size();
-            tmpFile = FileFactory.getTemporaryFile("trash_files.muco", false);
-            tmpOut = new OutputStreamWriter(tmpFile.getOutputStream(false), "utf-8");
-
+            appleScript = "tell application \"Finder\" to move {";
             for(int i=0; i<nbFiles; i++) {
-                tmpOut.write(((AbstractFile)queuedFiles.elementAt(i)).getAbsolutePath());
+                appleScript += "posix file \""+((AbstractFile)queuedFiles.elementAt(i)).getAbsolutePath()+"\"";
                 if(i<nbFiles-1)
-                    tmpOut.write("\n");
+                    appleScript += ", ";
             }
+            appleScript += "} to the trash";
 
-            tmpOut.close();
-
-            // Set the 'tmpFilePath' variable to the path of the temporary file we just created
-            String appleScript = "set tmpFilePath to \""+tmpFile.getAbsolutePath()+"\"\n";
-            appleScript += MOVE_TO_TRASH_APPLESCRIPT;
-
-            boolean success = executeAppleScript(appleScript, null);
-
-            // AppleScript has been executed, we can now safely close and delete the temporary file
-            tmpFile.delete();
-
-            return success;
+            return AppleScript.execute(appleScript, null);
         }
-        catch(Exception e) {
-            if(Debug.ON) Debug.trace("Caught exception: "+e);
+        // Script for AppleScript versions without Unicode support (AppleScript 1.10 / Mac OS X 10.4 or lower)
+        else {
+            AbstractFile tmpFile = null;
+            OutputStreamWriter tmpOut = null;
 
-            if(tmpOut!=null) {
-                try {
-                    tmpOut.close();
+            try {
+                // Create the temporary file that contains the list of files to move, encoded as UTF-8 and separated by
+                // EOL characters. The file must NOT end with a trailing EOL.
+                int nbFiles = queuedFiles.size();
+                tmpFile = FileFactory.getTemporaryFile("trash_files.muco", false);
+                tmpOut = new OutputStreamWriter(tmpFile.getOutputStream(false), "utf-8");
+
+                for(int i=0; i<nbFiles; i++) {
+                    tmpOut.write(((AbstractFile)queuedFiles.elementAt(i)).getAbsolutePath());
+                    if(i<nbFiles-1)
+                        tmpOut.write("\n");
                 }
-                catch(IOException e1) {}
-            }
 
-            if(tmpFile!=null) {
-                try {
-                    tmpFile.delete();
+                tmpOut.close();
+
+                // Set the 'tmpFilePath' variable to the path of the temporary file we just created
+                appleScript = "set tmpFilePath to \""+tmpFile.getAbsolutePath()+"\"\n";
+                appleScript += MOVE_TO_TRASH_APPLESCRIPT_NO_UNICODE;
+
+                boolean success = AppleScript.execute(appleScript, null);
+
+                // AppleScript has been executed, we can now safely close and delete the temporary file
+                tmpFile.delete();
+
+                return success;
+            }
+            catch(IOException e) {
+                if(Debug.ON) Debug.trace("Caught IOException: "+e);
+
+                if(tmpOut!=null) {
+                    try { tmpOut.close(); }
+                    catch(IOException e1) {
+                        // There's not much we can do about it
+                    }
                 }
-                catch(IOException e2) {}
-            }
 
-            return false;
+                if(tmpFile!=null) {
+                    try { tmpFile.delete(); }
+                    catch(IOException e2) {
+                        // There's not much we can do about it
+                    }
+                }
+
+                return false;
+            }
         }
+
     }
 }
