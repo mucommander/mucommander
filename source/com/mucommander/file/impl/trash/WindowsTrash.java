7c25803de121f197eb00267f735c5776bb4af8d9
Maxence Bernard <maxence@maxence.info>
2008-03-01 00:20:25 +0000

    Now implements QueuedTrash instead of AbstractTrash for better performance.

--- source/com/mucommander/file/impl/trash/WindowsTrash.java
+++ source/com/mucommander/file/impl/trash/WindowsTrash.java
@@ -20,19 +20,23 @@ package com.mucommander.file.impl.trash;
 
 import com.mucommander.PlatformManager;
 import com.mucommander.file.AbstractFile;
-import com.mucommander.file.AbstractTrash;
 import com.mucommander.file.impl.local.LocalFile;
 import com.mucommander.file.impl.local.SpecialWindowsLocation;
 import com.mucommander.file.util.Shell32API;
 
+import java.util.Vector;
+
 
 /**
  * WindowsTrash is an <code>AbstractTrash</code> implementation for the <i>Microsoft Windows' Recycle Bin</i>.
- * Native methods in the Shell32 DLL are used to access the Recycle Bin.
+ *
+ * <p>Native methods in the Shell32 Windows API are used to access the Recycle Bin. There is an overhead associated with
+ * invoking those methods (via JNA), so for performance reasons, this trash is implemented as a {@link com.mucommander.file.impl.trash.QueuedTrash}
+ * in order to group calls to {@link #moveToTrash(com.mucommander.file.AbstractFile)}.</p> 
  *
  * @author Maxence Bernard
  */
-public class WindowsTrash extends AbstractTrash {
+public class WindowsTrash extends QueuedTrash {
 
     //////////////////////////////////
     // AbstractTrash implementation //
@@ -43,20 +47,6 @@ public class WindowsTrash extends AbstractTrash {
      */
     public boolean canMoveToTrash(AbstractFile file) {
         return file.getTopAncestor() instanceof LocalFile;
-    }
-
-    public boolean moveToTrash(AbstractFile file) {
-        Shell32API.SHFILEOPSTRUCT fileop = new Shell32API.SHFILEOPSTRUCT();
-
-        fileop.wFunc = Shell32API.FO_DELETE;
-        fileop.fFlags = Shell32API.FOF_ALLOWUNDO| Shell32API.FOF_NOCONFIRMATION| Shell32API.FOF_SILENT;
-        // Directories (and regular files) must not end with a trailing slash or the operation will fail.
-        // The encodePaths method takes care of encoding the path in a special way.
-        fileop.pFrom = fileop.encodePaths(new String[]{file.getAbsolutePath(false)}); 
-
-        // Note: if needed, SHFileOperation is able to move a list of files to the trash instead of just one.                                
-
-        return Shell32API.INSTANCE.SHFileOperation(fileop) == 0;
     }
 
     /**
@@ -115,9 +105,28 @@ public class WindowsTrash extends AbstractTrash {
         PlatformManager.openInFileManager(SpecialWindowsLocation.RECYCLE_BIN);
     }
 
-    /**
-     * Implementation notes: this method is implemented as a no-op.
-     */
-    public void waitForPendingOperations() {
+
+    ////////////////////////////////
+    // QueuedTrash implementation //
+    ////////////////////////////////
+
+    protected boolean moveToTrash(Vector queuedFiles) {
+        Shell32API.SHFILEOPSTRUCT fileop = new Shell32API.SHFILEOPSTRUCT();
+
+        fileop.wFunc = Shell32API.FO_DELETE;
+        fileop.fFlags = Shell32API.FOF_ALLOWUNDO| Shell32API.FOF_NOCONFIRMATION| Shell32API.FOF_SILENT;
+
+        int nbFiles = queuedFiles.size();
+
+        String[] paths = new String[nbFiles];
+        for(int i=0; i<nbFiles; i++) {
+            // Directories (and regular files) must not end with a trailing slash or the operation will fail.
+            paths[i] = ((AbstractFile)queuedFiles.elementAt(i)).getAbsolutePath(false);
+        }
+
+        // The encodePaths method takes care of encoding the paths in a special way.
+        fileop.pFrom = fileop.encodePaths(paths);
+
+        return Shell32API.INSTANCE.SHFileOperation(fileop) == 0;
     }
 }
