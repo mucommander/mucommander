429d9ad3985aded8552bc7e5c8d6b777ea4e829d
Maxence Bernard <maxence@maxence.info>
2008-10-21 10:30:08 +0000

    Fix for bug #89 (menu bar selected on Alt+Tab)

--- source/com/mucommander/ui/main/WindowManager.java
+++ source/com/mucommander/ui/main/WindowManager.java
@@ -561,10 +561,16 @@ public class WindowManager implements WindowListener, ConfigurationListener {
     public void windowDeactivated(WindowEvent e) {
         Object source = e.getSource();
 
+        // Workaround for JRE bug #4841881 (http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4841881) /
+        // which causes Alt+Tab to focus the menu bar under certain L&F.
+        // This bug has also been reported as muCommmander bug #89.  
+        MenuSelectionManager.defaultManager().clearSelectedPath();
+
         // Return if event doesn't originate from a MainFrame (e.g. ViewerFrame or EditorFrame)
         if(!(source instanceof MainFrame))
             return;
 
+        // Let MainFrame know that it is not active anymore
         ((MainFrame)e.getSource()).setForegroundActive(false);
     }
 
