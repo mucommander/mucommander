8f9bd689e7e99c24ce2886b6262e84826365d5c8
Arik Hadas <arik.hadas1@gmail.com>
2009-07-24 22:44:06 +0000

    - Introduce ActionDescriptor class. - Introduce ActionCategory class.

---
+++ source/com/mucommander/ui/action/ActionDescriptor.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ * Copyright (C) 2002-2009 Maxence Bernard
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mucommander.ui.action;
+
+import javax.swing.ImageIcon;
+import javax.swing.KeyStroke;
+
+/**
+ * Each MuAction is registered with an object of ActionDescriptor type
+ * that provides its properties. ActionDescriptor is an interface that 
+ * defines those action's properties. 
+ * 
+ * @author Arik Hadas
+ */
+public interface ActionDescriptor {
+	
+	public String getID();
+	
+	public ActionCategory getCategory();
+	
+	public String getLabel();
+	
+	public String getLabelKey();
+	
+	public KeyStroke getDefaultKeyStroke();
+	
+	public KeyStroke getDefaultAltKeyStroke();
+	
+	public ImageIcon getIcon();
+	
+	public String getTooltip();
+}
