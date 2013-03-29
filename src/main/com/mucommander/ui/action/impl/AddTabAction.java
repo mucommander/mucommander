7eacdff514c72edb42d80935a4581ea96e4fac35
Arik Hadas <arik.hadas1@gmail.com>
2013-03-29 12:49:56 +0000

    add an action of adding a tab. the new tab is added to the active panel, and its initial location is set to the location of the active tab in the panel.

---
+++ src/main/com/mucommander/ui/action/impl/AddTabAction.java
@@ -0,0 +1,70 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ * Copyright (C) 2002-2012 Maxence Bernard
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mucommander.ui.action.impl;
+
+import java.awt.event.KeyEvent;
+import java.util.Map;
+
+import javax.swing.KeyStroke;
+
+import com.mucommander.commons.file.AbstractFile;
+import com.mucommander.ui.action.AbstractActionDescriptor;
+import com.mucommander.ui.action.ActionCategories;
+import com.mucommander.ui.action.ActionCategory;
+import com.mucommander.ui.action.ActionFactory;
+import com.mucommander.ui.action.MuAction;
+import com.mucommander.ui.main.MainFrame;
+
+/**
+ * This action adds a new tab in the active panel with the location
+ * that is currently presented in the active tab.
+ *
+ * @author Arik Hadas
+ */
+public class AddTabAction extends MuAction {
+
+	public AddTabAction(MainFrame mainFrame, Map<String,Object> properties) {
+        super(mainFrame, properties);
+    }
+
+	@Override
+	public void performAction() {
+		AbstractFile currentFolder = mainFrame.getActivePanel().getCurrentFolder();
+		mainFrame.getActivePanel().getTabs().add(currentFolder);
+	}
+
+	public static class Factory implements ActionFactory {
+
+		public MuAction createAction(MainFrame mainFrame, Map<String,Object> properties) {
+			return new AddTabAction(mainFrame, properties);
+		}
+    }
+
+	public static class Descriptor extends AbstractActionDescriptor {
+    	public static final String ACTION_ID = "AddTab";
+    	
+		public String getId() { return ACTION_ID; }
+
+		public ActionCategory getCategory() { return ActionCategories.TAB; }
+
+		public KeyStroke getDefaultAltKeyStroke() { return null; }
+
+		public KeyStroke getDefaultKeyStroke() { return KeyStroke.getKeyStroke(KeyEvent.VK_T, KeyEvent.SHIFT_DOWN_MASK | KeyEvent.CTRL_DOWN_MASK); }
+    }
+}
