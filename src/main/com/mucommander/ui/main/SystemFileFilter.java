e28ceb580f05f5850c66e05c588e3acaab642c2f
Arik Hadas <arik.hadas1@gmail.com>
2013-07-12 20:25:31 +0000

    remove the array of system folders in MAC OS from SystemFileFilter and change SystemFileFilter#accept method to use AbstractFile#isSystem method

--- src/main/com/mucommander/ui/main/SystemFileFilter.java
+++ src/main/com/mucommander/ui/main/SystemFileFilter.java
@@ -31,64 +31,11 @@ import com.mucommander.commons.file.filter.AbstractFileFilter;
  */
 public class SystemFileFilter extends AbstractFileFilter {
 
-    /**
-     * Top-level Mac OS X system folders hidden by Finder. For more info about those files:
-     * http://www.westwind.com/reference/OS-X/invisibles.html
-     */
-    private final static String SYSTEM_FOLDERS[]= {
-        // Mac OS X system folders
-        "/.Trashes",
-        "/.vol",
-        "/dev",
-        "/automount",
-        "/bin",
-        "/cores",
-        "/etc",
-        "/lost+found",    
-        "/Network",
-        "/private",
-        "/sbin",
-        "/tmp",
-        "/usr",
-        "/var",
-//        "/Volumes",
-        "/mach.sym",
-        "/mach_kernel",
-        "/mach",
-        "/Desktop DB",
-        "/Desktop DF",
-        "/File Transfer Folder",
-        "/.hotfiles.btree",
-        "/.Spotlight-V100",
-        "/.hidden",     // Used by Mac OS X up to 10.3, not in 10.4
-        System.getProperty("user.home")+"/.Trash",  // User trash folder
-        // Mac OS 9 system folders 
-        "/AppleShare PDS",
-        "/Cleanup At Startup",
-        "/Desktop Folder",
-        "/Network Trash Folder",
-        "/Shutdown Check",
-        "/Temporary Items",
-        System.getProperty("user.home")+"/Temporary Items",  // User trash folder
-        "/TheFindByContentFolder",
-        "/TheVolumeSettingsFolder",
-        "/Trash",
-        "/VM Storage"
-    };
-
-
     ///////////////////////////////
     // FileFilter implementation //
     ///////////////////////////////
 
     public boolean accept(AbstractFile file) {
-        String path = file.getAbsolutePath(false);
-
-        int nbSystemFolders = SYSTEM_FOLDERS.length;
-        for(int i=0; i<nbSystemFolders; i++)
-            if(path.equals(SYSTEM_FOLDERS[i]))
-                return false;
-
-        return true;
+        return file.isSystem();
     }
 }
