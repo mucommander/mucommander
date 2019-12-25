61e587c15a693ba6af613141fff0440fba1e157e
Arik Hadas <arik.hadas1@gmail.com>
2019-12-25 16:25:25 +0200

    fix about, preferences and quit handlers on macOS

--- mucommander-core/src/main/java/com/mucommander/ui/macosx/EAWTHandler.java
+++ mucommander-core/src/main/java/com/mucommander/ui/macosx/EAWTHandler.java
@@ -16,76 +16,53 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 package com.mucommander.ui.macosx;
 
-import com.apple.eawt.Application;
-import com.apple.eawt.ApplicationEvent;
-import com.apple.eawt.ApplicationListener;
+import java.awt.Desktop;
+import java.awt.desktop.OpenFilesEvent;
+import java.awt.desktop.QuitEvent;
+import java.awt.desktop.QuitResponse;
+
 import com.mucommander.muCommander;
 import com.mucommander.commons.file.AbstractFile;
 import com.mucommander.commons.file.FileFactory;
 import com.mucommander.ui.main.FolderPanel;
 import com.mucommander.ui.main.WindowManager;
 
-
 /**
- * This class registers the About, Preferences and Quit handlers using the com.apple.eawt API available
- * under Java 1.4 and up.
+ * This class registers the About, Preferences and Quit handlers.
  *
- * @author Maxence Bernard
+ * @author Arik Hadas, Maxence Bernard
  */
-class EAWTHandler implements ApplicationListener {
+class EAWTHandler {
 
     public EAWTHandler() {
-        Application app = new Application();
-        // Enable the 'About' menu item
-        app.setEnabledAboutMenu(true);
-        // Enable the 'Preferences' menu item
-        app.setEnabledPreferencesMenu(true);
-        // Register this ApplicationListener
-        app.addApplicationListener(this);
+        Desktop desktop = Desktop.getDesktop();
+        desktop.setAboutHandler(e -> OSXIntegration.showAbout());
+        desktop.setPreferencesHandler(e -> OSXIntegration.showPreferences());
+        desktop.setQuitHandler(this::handleQuitRequestWith);
+        desktop.setOpenFileHandler(this::openFiles);
     }
 
-    public void handleAbout(ApplicationEvent event) {
-        event.setHandled(true);
-        OSXIntegration.showAbout();
+    public void handleQuitRequestWith(final QuitEvent e, final QuitResponse response) {
+        if (OSXIntegration.doQuit())
+            response.performQuit();
+        else
+            response.cancelQuit();
     }
 
-    public void handlePreferences(ApplicationEvent event) {
-        event.setHandled(true);
-        OSXIntegration.showPreferences();
-    }
-
-    public void handleQuit(ApplicationEvent event) {
-        // Accept or reject the request to quit based on user's response
-        event.setHandled(OSXIntegration.doQuit());
-    }
-
-    public void handleOpenApplication(ApplicationEvent event) {
-        // No-op
-    }
-
-    public void handleReOpenApplication(ApplicationEvent event) {
-        // No-op
-    }
-
-    public void handleOpenFile(ApplicationEvent event) {
-        // Wait until the application has been launched. This step is required to properly handle the case where the 
+    public void openFiles(final OpenFilesEvent e) {
+     // Wait until the application has been launched. This step is required to properly handle the case where the
         // application is launched with a file to open, for instance when drag-n-dropping a file to the Dock icon
         // when muCommander is not started yet. In this case, this method is called while Launcher is still busy
         // launching the application (no mainframe exists yet).
         muCommander.waitUntilLaunched();
 
-        AbstractFile file = FileFactory.getFile(event.getFilename());
+        AbstractFile file = FileFactory.getFile(e.getFiles().get(0).getAbsolutePath());
         FolderPanel activePanel = WindowManager.getCurrentMainFrame().getActivePanel();
-        if(file.isBrowsable())
+        if (file.isBrowsable())
             activePanel.tryChangeCurrentFolder(file);
         else
             activePanel.tryChangeCurrentFolder(file.getParent(), file, false);
-    }
-
-    public void handlePrintFile(ApplicationEvent event) {
-        // No-op
     }
 }
