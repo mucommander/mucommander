619e9704d4a93cf0a47dac17c25301ef0af69104
Arik Hadas <arik.hadas1@gmail.com>
2019-12-26 23:17:27 +0200

    cleanup in TransferableFileSet

--- mucommander-core/src/main/java/com/mucommander/ui/dnd/TransferableFileSet.java
+++ mucommander-core/src/main/java/com/mucommander/ui/dnd/TransferableFileSet.java
@@ -228,40 +228,36 @@ public class TransferableFileSet implements Transferable {
      */
     public static FileSet getTransferFiles(Transferable transferable) {
         FileSet files;
-        AbstractFile file;
 
         try {
             // FileSet DataFlavor
-            if(transferable.isDataFlavorSupported(FILE_SET_DATA_FLAVOR)) {
+            if (transferable.isDataFlavorSupported(FILE_SET_DATA_FLAVOR)) {
                 files = (FileSet)transferable.getTransferData(FILE_SET_DATA_FLAVOR);
             }
             // File list DataFlavor
-            else if(transferable.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+            else if (transferable.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
                 List<File> fileList = (List<File>)transferable.getTransferData(DataFlavor.javaFileListFlavor);
 
                 int nbFiles = fileList.size();
                 files = new FileSet();
                 for(int i=0; i<nbFiles; i++) {
-                    file = FileFactory.getFile(fileList.get(i).getAbsolutePath());
+                    AbstractFile file = FileFactory.getFile(fileList.get(i).getAbsolutePath());
 
                     if(file!=null)
                         files.add(file);
                 }
             }
             // Text plain DataFlavor: assume that lines designate file paths
-            else if(transferable.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-                BufferedReader br;
-
-                br = null;
-                try {
-                    br = new BufferedReader(DataFlavor.getTextPlainUnicodeFlavor().getReaderForText(transferable));
-
+            else if (transferable.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                // Documentation is not explicit on whether DataFlavor streams need to be closed, we might as well
+                // do so just to be sure.
+                try(BufferedReader br = new BufferedReader(DataFlavor.getTextPlainUnicodeFlavor().getReaderForText(transferable))) {
                     // Read input line by line and try to create AbstractFile instances
                     String path;
                     files = new FileSet();
                     while((path=br.readLine())!=null) {
                         // Try to create an AbstractFile instance, returned instance may be null
-                        file = FileFactory.getFile(path);
+                        AbstractFile file = FileFactory.getFile(path);
 
                         // Safety precaution: if at least one line doesn't resolve as a file, stop reading
                         // and return null. This is to avoid any nasty effect that could arise if a random
@@ -270,14 +266,6 @@ public class TransferableFileSet implements Transferable {
                             return null;
 
                         files.add(file);
-                    }
-                }
-                // Documentation is not explicit on whether DataFlavor streams need to be closed, we might as well
-                // do so just to be sure.
-                finally {
-                    if(br != null) {
-                        try {br.close();}
-                        catch(IOException e) {}
                     }
                 }
             }
