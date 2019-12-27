2e6c01b6eedb517f236fd27bdd497ba1d2dc7b31
Arik Hadas <arik.hadas1@gmail.com>
2019-12-27 15:49:31 +0200

    cleanup related to drag and drop

--- mucommander-core/src/main/java/com/mucommander/ui/dnd/DnDContext.java
+++ mucommander-core/src/main/java/com/mucommander/ui/dnd/DnDContext.java
@@ -85,12 +85,5 @@ public class DnDContext {
      */
     static void setDragGestureModifiersEx(int modifiersEx) {
         dragGestureModifiersEx = modifiersEx;
-
-//        AppLogger.finest("gestureModifiersEx="+modifiersEx);
-//        AppLogger.finest("getModifiersExText="+ InputEvent.getModifiersExText(modifiersEx));
-//        AppLogger.finest("is shift down="+((modifiersEx&InputEvent.SHIFT_DOWN_MASK)!=0));
-//        AppLogger.finest("is ctrl down="+((modifiersEx&InputEvent.CTRL_DOWN_MASK)!=0));
-//        AppLogger.finest("is alt down="+((modifiersEx&InputEvent.ALT_DOWN_MASK)!=0));
-//        AppLogger.finest("is meta down="+((modifiersEx&InputEvent.META_DOWN_MASK)!=0));
     }
 }
--- mucommander-core/src/main/java/com/mucommander/ui/dnd/FileDropTargetListener.java
+++ mucommander-core/src/main/java/com/mucommander/ui/dnd/FileDropTargetListener.java
@@ -44,36 +44,40 @@ import com.mucommander.ui.main.FolderPanel;
 import com.mucommander.ui.main.MainFrame;
 
 /**
- * Provides file(s) 'drop' support to components that add a <code>DropTarget</code> using this <code>DropTargetListener</code>.
- * A {@link com.mucommander.ui.main.FolderPanel} instance has to be specified at creation time, this instance will be
- * used to change the current folder, or copy/move files to the current folder.
+ * Provides file(s) 'drop' support to components that add a <code>DropTarget</code> using this
+ * <code>DropTargetListener</code>. A {@link com.mucommander.ui.main.FolderPanel} instance has to be specified at
+ * creation time, this instance will be used to change the current folder, or copy/move files to the current folder.
  *
- * <p>There are 2 different modes this class can operate in. The mode to be used has to be specified when this class is
+ * <p>
+ * There are 2 different modes this class can operate in. The mode to be used has to be specified when this class is
  * instantiated.
  *
- * <p>In 'folder change mode', when a file or string representing a file path is dropped, the associated FolderPanel's
+ * <p>
+ * In 'folder change mode', when a file or string representing a file path is dropped, the associated FolderPanel's
  * current folder is changed:
  * <ul>
  * <li>If the file is a directory, the current folder is changed to that directory
- * <li>For any other file kind (archive, regular file...), current folder is changed to the file's parent folder
- * and the file is selected
+ * <li>For any other file kind (archive, regular file...), current folder is changed to the file's parent folder and the
+ * file is selected
  * </ul>
  * If more than one file (or file path) is dropped, only the first one is taken into account.
  *
- * <p>In the normal mode, files (or file paths) that are dropped can also be moved or copied to the associated FolderPanel's
- * current folder, on top of the change current folder action. The actual drop action performed (move, copy or change current folder)
- * depends on the keyboard modifiers typed by the user when dragging the files.
- * When the mouse cursor enters the drop-enabled component's area, it is changed to symbolize the action to be performed.
- * The default drop action (when no modifier is down) is copy. 
+ * <p>
+ * In the normal mode, files (or file paths) that are dropped can also be moved or copied to the associated
+ * FolderPanel's current folder, on top of the change current folder action. The actual drop action performed (move,
+ * copy or change current folder) depends on the keyboard modifiers typed by the user when dragging the files. When the
+ * mouse cursor enters the drop-enabled component's area, it is changed to symbolize the action to be performed. The
+ * default drop action (when no modifier is down) is copy.
  *
- * <p>Drop events originating from the same FolderPanel are on purpose not accepted as spring-loaded folders are not
- * (yet) supported which would make the drop operation ambiguous and confusing.
+ * <p>
+ * Drop events originating from the same FolderPanel are on purpose not accepted as spring-loaded folders are not (yet)
+ * supported which would make the drop operation ambiguous and confusing.
  *
- * @author Maxence Bernard
+ * @author Maxence Bernard, Arik Hadas
  */
 public class FileDropTargetListener implements DropTargetListener {
-	private static final Logger LOGGER = LoggerFactory.getLogger(FileDropTargetListener.class);
-	
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileDropTargetListener.class);
+
     /** the FolderPanel instance used to change the current folder when a file is dropped */
     private FolderPanel folderPanel;
 
@@ -91,67 +95,65 @@ public class FileDropTargetListener implements DropTargetListener {
      * <code>InputEvent.META_DOWN_MASK</code> under Mac OS X, <code>InputEvent.ALT_DOWN_MASK</code> under any other
      * platform.
      */
-    private final static int MOVE_ACTION_MODIFIERS_EX = OsFamily.MAC_OS_X.isCurrent()?
-            InputEvent.META_DOWN_MASK
-            :InputEvent.ALT_DOWN_MASK;
-
+    private final static int MOVE_ACTION_MODIFIERS_EX = OsFamily.MAC_OS_X.isCurrent() ? InputEvent.META_DOWN_MASK
+            : InputEvent.ALT_DOWN_MASK;
 
     /**
      * Creates a new FileDropTargetListener using the provided FolderPanel that will be used to either change the
      * current folder or copy/move when files are dropped, depending on the specified operating mode and drop action.
      *
-     * @param folderPanel the FolderPanel instance used to change the current folder or copy/move when files are dropped
-     * @param changeFolderOnlyMode if <code>true</code>, the FolderPanel's current folder can only be changed when file(s)
-     * are dropped, files cannot be copied or moved.
+     * @param folderPanel
+     *            the FolderPanel instance used to change the current folder or copy/move when files are dropped
+     * @param changeFolderOnlyMode
+     *            if <code>true</code>, the FolderPanel's current folder can only be changed when file(s) are dropped,
+     *            files cannot be copied or moved.
      */
     public FileDropTargetListener(FolderPanel folderPanel, boolean changeFolderOnlyMode) {
         this.folderPanel = folderPanel;
         this.changeFolderOnlyMode = changeFolderOnlyMode;
     }
 
-
     /**
-     * Returns a mouse <code>Cursor<code> that symbolizes the given drop action and 'accepted' status.
-     * The given action must one of the following:
+     * Returns a mouse <code>Cursor<code> that symbolizes the given drop action and 'accepted' status. The given action
+     * must one of the following:
      * <ul>
-     *  <li>DnDConstants.ACTION_COPY
-     *  <li>DnDConstants.ACTION_MOVE
-     *  <li>DnDConstants.ACTION_LINK
+     * <li>DnDConstants.ACTION_COPY
+     * <li>DnDConstants.ACTION_MOVE
+     * <li>DnDConstants.ACTION_LINK
      * </ul>
-     * If the action has any other value, the default Cursor is returned. 
+     * If the action has any other value, the default Cursor is returned.
      */
     private Cursor getDragActionCursor(int dropAction, boolean dragAccepted) {
-        switch(dropAction) {
-            case DnDConstants.ACTION_COPY:
-                return dragAccepted?DragSource.DefaultCopyDrop:DragSource.DefaultCopyNoDrop;
+        switch (dropAction) {
+        case DnDConstants.ACTION_COPY:
+            return dragAccepted ? DragSource.DefaultCopyDrop : DragSource.DefaultCopyNoDrop;
 
-            case DnDConstants.ACTION_MOVE:
-                return dragAccepted?DragSource.DefaultMoveDrop:DragSource.DefaultMoveNoDrop;
+        case DnDConstants.ACTION_MOVE:
+            return dragAccepted ? DragSource.DefaultMoveDrop : DragSource.DefaultMoveNoDrop;
 
-            case DnDConstants.ACTION_LINK:
-                return dragAccepted?DragSource.DefaultLinkDrop:DragSource.DefaultLinkNoDrop;
+        case DnDConstants.ACTION_LINK:
+            return dragAccepted ? DragSource.DefaultLinkDrop : DragSource.DefaultLinkNoDrop;
 
-            default:
-                return Cursor.getDefaultCursor();
+        default:
+            return Cursor.getDefaultCursor();
         }
     }
 
-
     /**
      * Accepts or rejects the specified <code>DropTargetDragEvent</code> and changes the mouse cursor to match the
-     * current drop action.
-     * The drag event will be accepted it supports at least one of the supported DataFlavors and one of the two
-     * following conditions are true:
+     * current drop action. The drag event will be accepted it supports at least one of the supported DataFlavors and
+     * one of the two following conditions are true:
      * <ul>
      * <li>the event originates from one of muCommander's {@link FolderPanel} for which the current folder is not the
      * same as the FolderPanel associated with this <code>FileDropTargetListener</code>
      * <li>the event does not originate from muCommander
      * </ul>
      *
-     * <p>This method overrides the default drop action for drag-and-drop operations within muCommander to make it
-     * <code>DnDConstants.ACTION_COPY</code> instead of <code>DnDConstants.ACTION_MOVE</code>.
-     * For a move action to be performed when the mouse is released, the modifiers defined by
-     * {@link #MOVE_ACTION_MODIFIERS_EX} must be down.</p>
+     * <p>
+     * This method overrides the default drop action for drag-and-drop operations within muCommander to make it
+     * <code>DnDConstants.ACTION_COPY</code> instead of <code>DnDConstants.ACTION_MOVE</code>. For a move action to be
+     * performed when the mouse is released, the modifiers defined by {@link #MOVE_ACTION_MODIFIERS_EX} must be down.
+     * </p>
      *
      * @return <code>true</code> if the event was accepted, false otherwise
      */
@@ -162,18 +164,17 @@ public class FileDropTargetListener implements DropTargetListener {
             this.currentDropAction = determineDropAction(event);
         }
 
-        LOGGER.trace("dragAccepted="+dragAccepted+" dropAction="+currentDropAction);
+        LOGGER.trace("dragAccepted=" + dragAccepted + " dropAction=" + currentDropAction);
 
-        if(dragAccepted) {
+        if (dragAccepted) {
             // Accept the drag event with our drop action
             event.acceptDrag(currentDropAction);
-        }
-        else {
+        } else {
             // Reject the drag event
             event.rejectDrag();
         }
 
-        LOGGER.trace("cursor="+getDragActionCursor(currentDropAction, dragAccepted));
+        LOGGER.trace("cursor=" + getDragActionCursor(currentDropAction, dragAccepted));
 
         // Change the mouse cursor on this FolderPanel and child components
         folderPanel.setCursor(getDragActionCursor(currentDropAction, dragAccepted));
@@ -188,7 +189,7 @@ public class FileDropTargetListener implements DropTargetListener {
             // if the move extended modifiers are not currently down.
             int dragModifiers = DnDContext.getDragGestureModifiersEx();
 
-            if(dropAction==DnDConstants.ACTION_MOVE
+            if (dropAction == DnDConstants.ACTION_MOVE
                     && (dragModifiers & MOVE_ACTION_MODIFIERS_EX) == 0
                     && (event.getSourceActions() & DnDConstants.ACTION_COPY) != 0) {
                 LOGGER.debug("changing default action, was: DnDConstants.ACTION_MOVE, now: DnDConstants.ACTION_COPY");
@@ -215,7 +216,8 @@ public class FileDropTargetListener implements DropTargetListener {
     }
 
     private boolean isPointToTargetFolder(FolderPanel dragInitiator) {
-        return dragInitiator==folderPanel || dragInitiator.getCurrentFolder().equalsCanonical(folderPanel.getCurrentFolder());
+        return dragInitiator == folderPanel
+                || dragInitiator.getCurrentFolder().equalsCanonical(folderPanel.getCurrentFolder());
     }
 
     ////////////////////////////////////
@@ -226,24 +228,20 @@ public class FileDropTargetListener implements DropTargetListener {
         acceptOrRejectDragEvent(event);
     }
 
-
     public void dragOver(DropTargetDragEvent event) {
         // Although it doesn't look necessary, cursor needs to be set each time this method is called otherwise
         // it returns to the default one (at least under Mac OS X w/ Java 1.5)
         acceptOrRejectDragEvent(event);
     }
 
-
     public void dropActionChanged(DropTargetDragEvent event) {
         acceptOrRejectDragEvent(event);
     }
-
 
     public void dragExit(DropTargetEvent event) {
         // Restore default cursor
         folderPanel.setCursor(Cursor.getDefaultCursor());
     }
-
 
     public void drop(DropTargetDropEvent event) {
         // Restore default cursor, no matter what
@@ -251,7 +249,7 @@ public class FileDropTargetListener implements DropTargetListener {
 
         // The drop() method is called even if a DropTargetDropEvent was rejected before,
         // so this test is really necessary
-        if(!dragAccepted) {
+        if (!dragAccepted) {
             event.rejectDrop();
             return;
         }
@@ -259,11 +257,12 @@ public class FileDropTargetListener implements DropTargetListener {
         // Accept drop event
         event.acceptDrop(currentDropAction);
 
-        // Retrieve the files contained by the transferable as a FileSet (takes care of handling the different DataFlavors)
+        // Retrieve the files contained by the transferable as a FileSet (takes care of handling the different
+        // DataFlavors)
         FileSet droppedFiles = TransferableFileSet.getTransferFiles(event.getTransferable());
 
         // Stop and report failure if no file could not be retrieved
-        if(droppedFiles==null || droppedFiles.size()==0) {
+        if (droppedFiles == null || droppedFiles.size() == 0) {
             // Report drop failure
             event.dropComplete(false);
 
@@ -273,13 +272,14 @@ public class FileDropTargetListener implements DropTargetListener {
         // If in 'change folder mode' or if the drop action is 'ACTION_LINK' in normal mode:
         // change the FolderPanel's current folder to the dropped file/folder :
         // - If the file is a directory, the current folder is changed to that directory
-        // - For any other file kind (archive, regular file...), current folder is changed to the file's parent folder and the file is selected
+        // - For any other file kind (archive, regular file...), current folder is changed to the file's parent folder
+        // and the file is selected
         // If more than one file is dropped, only the first one is used
-        if(changeFolderOnlyMode || currentDropAction==DnDConstants.ACTION_LINK) {
+        if (changeFolderOnlyMode || currentDropAction == DnDConstants.ACTION_LINK) {
             AbstractFile file = droppedFiles.elementAt(0);
 
             // If file is a directory, change current folder to that directory
-            if(file.isDirectory())
+            if (file.isDirectory())
                 folderPanel.tryChangeCurrentFolder(file);
             // For any other file kind (archive, regular file...), change directory to the file's parent folder
             // and select the file
@@ -293,16 +293,27 @@ public class FileDropTargetListener implements DropTargetListener {
         else {
             MainFrame mainFrame = folderPanel.getMainFrame();
             AbstractFile destFolder = folderPanel.getCurrentFolder();
-            if(currentDropAction==DnDConstants.ACTION_MOVE) {
+            if (currentDropAction == DnDConstants.ACTION_MOVE) {
                 // Start moving files
                 ProgressDialog progressDialog = new ProgressDialog(mainFrame, Translator.get("move_dialog.moving"));
-                MoveJob moveJob = new MoveJob(progressDialog, mainFrame, droppedFiles, destFolder, null, FileCollisionDialog.ASK_ACTION, false);
+                MoveJob moveJob = new MoveJob(progressDialog,
+                        mainFrame,
+                        droppedFiles,
+                        destFolder,
+                        null,
+                        FileCollisionDialog.ASK_ACTION,
+                        false);
                 progressDialog.start(moveJob);
-            }
-            else {
+            } else {
                 // Start copying files
                 ProgressDialog progressDialog = new ProgressDialog(mainFrame, Translator.get("copy_dialog.copying"));
-                CopyJob job = new CopyJob(progressDialog, mainFrame, droppedFiles, destFolder, null, TransferMode.COPY, FileCollisionDialog.ASK_ACTION);
+                CopyJob job = new CopyJob(progressDialog,
+                        mainFrame,
+                        droppedFiles,
+                        destFolder,
+                        null,
+                        TransferMode.COPY,
+                        FileCollisionDialog.ASK_ACTION);
                 progressDialog.start(job);
             }
         }
