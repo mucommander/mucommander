6e873c78db99d1be58d7f8c791ed66be89dbdea0
Arik Hadas <arik.hadas1@gmail.com>
2020-01-18 14:49:48 +0200

    remove copyright from source code

--- mucommander-core/src/main/java/com/mucommander/ui/main/table/FileTableCellRenderer.java
+++ mucommander-core/src/main/java/com/mucommander/ui/main/table/FileTableCellRenderer.java
@@ -1,7 +1,6 @@
 /*
 
  * This file is part of muCommander, http://www.mucommander.com
- * Copyright (C) 2002-2018 Maxence Bernard
  *
  * muCommander is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
