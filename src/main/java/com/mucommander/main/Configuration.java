77088c9a51c5aabe79903e1b8368612b9e27fa3b
Arik Hadas <arik.hadas1@gmail.com>
2021-04-02 22:02:03 +0300

    update javadocs in com.mucommander.main package

--- src/main/java/com/mucommander/main/Configuration.java
+++ src/main/java/com/mucommander/main/Configuration.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mucommander.main;
 
 import java.util.ArrayList;
@@ -8,6 +25,9 @@ import java.util.stream.Collectors;
 
 import com.beust.jcommander.Parameter;
 
+/**
+ * @author Arik Hadas
+ */
 public class Configuration {
 
     /** Whether or not to display verbose error messages. */
