f5604c6dd92eaed90037ecaa05a51a2ea21acd91
Arik Hadas <arik.hadas1@gmail.com>
2021-05-15 00:44:14 +0300

    refactoring in FileTableHeader

--- mucommander-core/src/main/java/com/mucommander/ui/main/table/FileTableHeader.java
+++ mucommander-core/src/main/java/com/mucommander/ui/main/table/FileTableHeader.java
@@ -17,28 +17,71 @@
 
 package com.mucommander.ui.main.table;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JSeparator;
+import javax.swing.table.JTableHeader;
+
 import com.mucommander.core.desktop.DesktopManager;
 import com.mucommander.ui.action.ActionManager;
 import com.mucommander.ui.action.impl.ToggleAutoSizeAction;
 import com.mucommander.ui.main.MainFrame;
 
-import javax.swing.*;
-import javax.swing.table.JTableHeader;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
 /**
  * @author Maxence Bernard
  */
-public class FileTableHeader extends JTableHeader implements MouseListener {
-
-    private FileTable table;
+public class FileTableHeader extends JTableHeader {
 
     public FileTableHeader(FileTable table) {
         super(table.getColumnModel());
 
-        this.table = table;
-        addMouseListener(this);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                table.requestFocus();
+
+                // One of the table headers was left-clicked, sort the table by the clicked column's criterion
+                if (DesktopManager.isLeftMouseButton(e)) {
+                    Column col = Column.valueOf(table.convertColumnIndexToModel(getColumnModel().getColumnIndexAtX(e.getX())));
+                    // If the table was already sorted by this criteria, reverse order
+                    if (table.getSortInfo().getCriterion()==col)
+                        table.reverseSortOrder();
+                    else
+                        table.sortBy(col);
+                }
+                // One of the table headers was right-clicked, popup a menu that offers to hide the column
+                else if (DesktopManager.isRightMouseButton(e)) {
+                    JPopupMenu popupMenu = new JPopupMenu();
+                    MainFrame mainFrame = table.getFolderPanel().getMainFrame();
+
+                    for(Column c : Column.values()) {
+                        if(c==Column.NAME)
+                            continue;
+
+                        JCheckBoxMenuItem checkboxMenuItem = new JCheckBoxMenuItem(ActionManager.getActionInstance(c.getToggleColumnActionId(), mainFrame));
+
+                        checkboxMenuItem.setSelected(table.isColumnEnabled(c));
+                        checkboxMenuItem.setEnabled(table.isColumnDisplayable(c));
+                        // Override the action's label to a shorter one
+                        checkboxMenuItem.setText(c.getLabel());
+
+                        popupMenu.add(checkboxMenuItem);
+                    }
+
+                    popupMenu.add(new JSeparator());
+
+                    JCheckBoxMenuItem checkboxMenuItem = new JCheckBoxMenuItem(ActionManager.getActionInstance(ToggleAutoSizeAction.Descriptor.ACTION_ID, mainFrame));
+                    checkboxMenuItem.setSelected(mainFrame.isAutoSizeColumnsEnabled());
+                    popupMenu.add(checkboxMenuItem);
+
+                    popupMenu.show(FileTableHeader.this, e.getX(), e.getY());
+                    popupMenu.setVisible(true);
+                }
+            }
+        });
     }
 
 
@@ -49,66 +92,5 @@ public class FileTableHeader extends JTableHeader implements MouseListener {
     @Override
     public boolean getReorderingAllowed() {
         return true;
-    }
-
-    
-    //////////////////////////////////
-    // MouseListener implementation //
-    //////////////////////////////////
-
-    public void mouseClicked(MouseEvent e) {
-        Column col = Column.valueOf(table.convertColumnIndexToModel(getColumnModel().getColumnIndexAtX(e.getX())));
-
-        table.requestFocus();
-
-        // One of the table headers was left-clicked, sort the table by the clicked column's criterion
-        if(DesktopManager.isLeftMouseButton(e)) {
-            // If the table was already sorted by this criteria, reverse order
-            if (table.getSortInfo().getCriterion()==col)
-                table.reverseSortOrder();
-            else
-                table.sortBy(col);
-        }
-        // One of the table headers was right-clicked, popup a menu that offers to hide the column
-        else if(DesktopManager.isRightMouseButton(e)) {
-            JPopupMenu popupMenu = new JPopupMenu();
-            MainFrame mainFrame = table.getFolderPanel().getMainFrame();
-
-            JCheckBoxMenuItem checkboxMenuItem;
-            for(Column c : Column.values()) {
-                if(c==Column.NAME)
-                    continue;
-
-                checkboxMenuItem = new JCheckBoxMenuItem(ActionManager.getActionInstance(c.getToggleColumnActionId(), mainFrame));
-
-                checkboxMenuItem.setSelected(table.isColumnEnabled(c));
-                checkboxMenuItem.setEnabled(table.isColumnDisplayable(c));
-                // Override the action's label to a shorter one
-                checkboxMenuItem.setText(c.getLabel());
-
-                popupMenu.add(checkboxMenuItem);
-            }
-
-            popupMenu.add(new JSeparator());
-
-            checkboxMenuItem = new JCheckBoxMenuItem(ActionManager.getActionInstance(ToggleAutoSizeAction.Descriptor.ACTION_ID, mainFrame));
-            checkboxMenuItem.setSelected(mainFrame.isAutoSizeColumnsEnabled());
-            popupMenu.add(checkboxMenuItem);
-
-            popupMenu.show(this, e.getX(), e.getY());
-            popupMenu.setVisible(true);
-        }
-    }
-
-    public void mousePressed(MouseEvent e) {
-    }
-
-    public void mouseReleased(MouseEvent e) {
-    }
-
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    public void mouseExited(MouseEvent e) {
     }
 }
