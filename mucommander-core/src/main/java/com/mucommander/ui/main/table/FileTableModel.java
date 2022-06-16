0600cd6551ec8e16f88fc6e60fbcd13c20ce8108
Arik Hadas <arik.hadas1@gmail.com>
2022-06-16 23:16:14 +0300

    fix #559: do not round file sizes < 1 KB to 1 KB

--- mucommander-core/src/main/java/com/mucommander/ui/main/table/FileTableModel.java
+++ mucommander-core/src/main/java/com/mucommander/ui/main/table/FileTableModel.java
@@ -124,7 +124,7 @@ public class FileTableModel extends AbstractTableModel {
      */
     static void setSizeFormat(boolean compactSize) {
         if(compactSize)
-            sizeFormat = SizeFormat.DIGITS_MEDIUM | SizeFormat.UNIT_SHORT | SizeFormat.ROUND_TO_KB;
+            sizeFormat = SizeFormat.DIGITS_MEDIUM | SizeFormat.UNIT_SHORT;
         else
             sizeFormat = SizeFormat.DIGITS_FULL;
 
