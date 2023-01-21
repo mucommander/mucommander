3a6a214bbecea6ab57c95633c35ca16d2381c7bf
hajdam <hajdam@users.sf.net>
2023-01-21 17:41:21 +0100

    Fix for animated gifs - 2

--- mucommander-viewer-image/src/main/java/com/mucommander/viewer/image/ui/ImageViewerPanel.java
+++ mucommander-viewer-image/src/main/java/com/mucommander/viewer/image/ui/ImageViewerPanel.java
@@ -8,23 +8,25 @@ import com.mucommander.ui.theme.ThemeManager;
 
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
-import javax.swing.JPanel;
+import javax.swing.JComponent;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.ImageObserver;
 
 /**
  * Image viewer UI.
  */
 @ParametersAreNonnullByDefault
-public class ImageViewerPanel extends JPanel implements ThemeListener {
+public class ImageViewerPanel extends JComponent implements ThemeListener {
 
     private Image image;
     private int imageWidth;
     private int imageHeight;
     private double zoomFactor;
     private Color backgroundColor;
+    private final ScallingImageObserver imageObserver = new ScallingImageObserver();
 
     public ImageViewerPanel() {
         backgroundColor = ThemeManager.getCurrentColor(Theme.EDITOR_BACKGROUND_COLOR);
@@ -54,7 +56,7 @@ public class ImageViewerPanel extends JPanel implements ThemeListener {
         this.image = image;
         imageWidth = image.getWidth(null);
         imageHeight = image.getHeight(null);
-        setSize(getPreferredSize());
+        notifySizeChanged();
     }
 
     public void close() {
@@ -72,19 +74,19 @@ public class ImageViewerPanel extends JPanel implements ThemeListener {
         int frameWidth = getWidth();
         int frameHeight = getHeight();
 
-        final int scaledWidth = getScaledWidth();
-        final int scaledHeight = getScaledHeight();
+        final int scaledWidth = getScaledX(imageWidth);
+        final int scaledHeight = getScaledY(imageHeight);
 
         final int offsetX = Math.max(0, (frameWidth - scaledWidth) / 2);
         final int offsetY = Math.max(0, (frameHeight - scaledHeight) / 2);
         g.drawImage(image, offsetX, offsetY, offsetX + scaledWidth, offsetY + scaledHeight,
-                0, 0, imageWidth, imageHeight, backgroundColor, this);
+                0, 0, imageWidth, imageHeight, backgroundColor, imageObserver);
     }
 
     @Nonnull
     @Override
     public synchronized Dimension getPreferredSize() {
-        return image == null ? new Dimension(320, 200) : new Dimension(getScaledWidth(), getScaledHeight());
+        return image == null ? new Dimension(320, 200) : new Dimension(getScaledX(imageWidth), getScaledY(imageHeight));
     }
 
     //////////////////////////////////
@@ -109,17 +111,53 @@ public class ImageViewerPanel extends JPanel implements ThemeListener {
     public void fontChanged(FontChangedEvent event) {
     }
 
-    private int getScaledWidth() {
-        if (image == null) {
-            return 0;
-        }
-        return (int) (zoomFactor * imageWidth);
+    private void notifySizeChanged() {
+        setSize(getPreferredSize());
     }
 
-    private int getScaledHeight() {
+    private int getScaledX(int x) {
         if (image == null) {
             return 0;
         }
-        return (int) (zoomFactor * imageHeight);
+        return (int) (zoomFactor * x);
+    }
+
+    private int getScaledY(int y) {
+        if (image == null) {
+            return 0;
+        }
+        return (int) (zoomFactor * y);
+    }
+
+    private class ScallingImageObserver implements ImageObserver {
+
+        @Override public boolean imageUpdate(Image img, int infoFlags, int x, int y, int width, int height) {
+            // Update image size when changed during GIF animation
+            if ((infoFlags & ImageObserver.WIDTH) > 0) {
+                imageWidth = image.getWidth(null);
+            }
+            if ((infoFlags & ImageObserver.HEIGHT) > 0) {
+                imageHeight = image.getHeight(null);
+            }
+            if ((infoFlags & (ImageObserver.WIDTH + ImageObserver.HEIGHT)) > 0) {
+                notifySizeChanged();
+            }
+
+            // Notify image updated, but adjust affected position according to current zoom level
+            int frameWidth = getWidth();
+            int frameHeight = getHeight();
+
+            final int scaledWidth = getScaledX(imageWidth);
+            final int scaledHeight = getScaledY(imageHeight);
+
+            final int offsetX = Math.max(0, (frameWidth - scaledWidth) / 2);
+            final int offsetY = Math.max(0, (frameHeight - scaledHeight) / 2);
+
+            int adjX = x + offsetX;
+            int adjY = y + offsetY;
+            int adjWidth = getScaledX((int) (width + zoomFactor + 1)) + offsetX;
+            int adjHeight = getScaledY((int) (height + zoomFactor + 1)) + offsetY;
+            return ImageViewerPanel.this.imageUpdate(img, infoFlags, adjX, adjY, adjWidth, adjHeight);
+        }
     }
 }
