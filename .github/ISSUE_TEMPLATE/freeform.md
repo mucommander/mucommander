b91f39fb9e270eb3da93e46338293ac7b031345e
Piotr Skowronek <piotr.skowronek@gmail.com>
2023-05-14 12:14:14 +0200

    Report bug: better bug report with prefilled versions and logs, added bug/feature templates

---
+++ .github/ISSUE_TEMPLATE/freeform.md
@@ -0,0 +1,10 @@
+---
+name: Freeform issue template
+about: Describe the issue for muCommander - please provide everything that could be useful, like logs, muCommander version, OS version, java version etc
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+
--- mucommander-core/src/main/java/com/mucommander/ui/action/impl/ReportBugAction.java
+++ mucommander-core/src/main/java/com/mucommander/ui/action/impl/ReportBugAction.java
@@ -17,12 +17,18 @@
 
 package com.mucommander.ui.action.impl;
 
+import java.net.URLEncoder;
 import java.util.Map;
 
+import com.mucommander.Activator;
+import com.mucommander.RuntimeConstants;
 import com.mucommander.desktop.ActionType;
+import com.mucommander.text.Translator;
 import com.mucommander.ui.action.AbstractActionDescriptor;
 import com.mucommander.ui.action.ActionCategory;
+import com.mucommander.ui.dialog.debug.LoggingEvent;
 import com.mucommander.ui.main.MainFrame;
+import com.mucommander.utils.MuLogging;
 
 /**
  * This action opens the mucommander.com bug repository URL in the system's default browser.
@@ -31,12 +37,55 @@ import com.mucommander.ui.main.MainFrame;
  */
 public class ReportBugAction extends OpenURLInBrowserAction {
 
+    private static final String NEW_BUG_FORMAT = "%s?" +
+            "labels=bug&" +
+            "template=bug_report.yml&" +
+            "title=[Bug]%20&" +
+            "version=%s&" +
+            "java-version=%s&" +
+            "os-version=%s&" +
+            "logs=%s";
+
     public ReportBugAction(MainFrame mainFrame, Map<String,Object> properties) {
         super(mainFrame, properties);
-
-        putValue(URL_PROPERTY_KEY, com.mucommander.RuntimeConstants.REPORT_BUG_URL);
     }
-    
+
+    @Override
+    public void performAction() {
+        Object url = com.mucommander.RuntimeConstants.REPORT_BUG_URL;
+        try {
+            // TODO remove - temporarily left for testing: uncomment to check on that test-repo
+            //url = "https://github.com/pskowronek/parametrized_issue_template/issues/new";
+            String ver = String.format(Activator.portable ? "%s (%s)" : "%s", RuntimeConstants.VERSION, Translator.get("portable"));
+            String java_ver = String.join(System.lineSeparator(),
+                    System.getProperty("java.version"),
+                    System.getProperty("java.vm.name"),
+                    System.getProperty("java.vm.version"),
+                    System.getProperty("java.vm.vendor"));
+            String os_ver = String.join(System.lineSeparator(),
+                    System.getProperty("os.name"),
+                    System.getProperty("os.version"),
+                    System.getProperty("os.arch"));
+
+            final LoggingEvent[] records = MuLogging.getDebugConsoleAppender().getLogRecords();
+            StringBuilder logRecords = new StringBuilder();
+            for (LoggingEvent record : records) {
+                logRecords.append(record.toString());
+                logRecords.append(System.lineSeparator());
+            }
+            String newBugUrl = String.format(NEW_BUG_FORMAT, url,
+                    URLEncoder.encode(ver, "UTF-8"),
+                    URLEncoder.encode(java_ver, "UTF-8"),
+                    URLEncoder.encode(os_ver, "UTF-8"),
+                    URLEncoder.encode(logRecords.toString(),"UTF-8"));
+            putValue(URL_PROPERTY_KEY, newBugUrl);
+        } catch (Exception e) {
+            LOGGER.error("Error while preparing a bug report, falling back to generic", e);
+            putValue(URL_PROPERTY_KEY, url);
+        }
+        super.performAction();
+    }
+
     public static class Descriptor extends AbstractActionDescriptor {
         public String getId() { return ActionType.ReportBug.toString(); }
 
