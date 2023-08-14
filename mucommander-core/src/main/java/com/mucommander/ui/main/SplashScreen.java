f432a42f07aea83d0be33bb403c2f056e16b847c
Piotr Skowronek <piotr.skowronek@gmail.com>
2023-08-14 22:47:12 +0200

    SplashScreen reformat

--- mucommander-core/src/main/java/com/mucommander/ui/main/SplashScreen.java
+++ mucommander-core/src/main/java/com/mucommander/ui/main/SplashScreen.java
@@ -24,13 +24,15 @@ import com.mucommander.ui.icon.IconManager;
 import javax.swing.*;
 import java.awt.*;
 
-
 /**
  * Splash screen that gets displayed on muCommander startup.
  *
- * <p>The splash screen is made of a logo image on top of which is displayed muCommander version number (in the top right corner)
- * and a loading message (in the lower left corner) which is updated by {@link com.mucommander.Application} to show startup progress. 
- * It is then closed by {@link com.mucommander.Application} when muCommander is fully started and ready for use.</p> 
+ * <p>
+ * The splash screen is made of a logo image on top of which is displayed muCommander version number (in the top right
+ * corner) and a loading message (in the lower left corner) which is updated by {@link com.mucommander.Application} to
+ * show startup progress. It is then closed by {@link com.mucommander.Application} when muCommander is fully started and
+ * ready for use.
+ * </p>
  *
  * @author Maxence Bernard
  */
@@ -46,17 +48,18 @@ public class SplashScreen extends JWindow {
     private Font customFont;
 
     /** Path to the splash screen logo image within the JAR file */
-    private final static String SPLASH_IMAGE_PATH = IconManager.getIconSetFolder(IconManager.MUCOMMANDER_ICON_SET)+"splash.png";
+    private final static String SPLASH_IMAGE_PATH =
+            IconManager.getIconSetFolder(IconManager.MUCOMMANDER_ICON_SET) + "splash.png";
 
     /** Name of the font used to display text on this splash screen */
     private final static String FONT_NAME = "Courier";
     /** Style of the font used to display text on this splash screen */
-//    private final static int FONT_STYLE = Font.PLAIN;
+    // private final static int FONT_STYLE = Font.PLAIN;
     private final static int FONT_STYLE = Font.BOLD;
     /** Size of the font used to display text on this splash screen */
     private final static int FONT_SIZE = 11;
 
-    /** Color of the text displayed on this splash screen */ 
+    /** Color of the text displayed on this splash screen */
     private final static Color TEXT_COLOR = new Color(192, 238, 241);
     private final static Color SHADOW_TEXT_COLOR = new Color(0, 86, 117);
 
@@ -70,12 +73,13 @@ public class SplashScreen extends JWindow {
     /** Number of pixels between the version information and the top of the splash image */
     private final static int VERSION_MARGIN_Y = 3;
 
-
     /**
      * Creates and displays a new SplashScreen, with the given version string and initial loading message.
      *
-     * @param version muCommander version string which will be displayed in the top right corner
-     * @param loadingMessage initial loading message, displayed in the lower left corner
+     * @param version
+     *            muCommander version string which will be displayed in the top right corner
+     * @param loadingMessage
+     *            initial loading message, displayed in the lower left corner
      */
     public SplashScreen(String version, String loadingMessage) {
         this.version = version;
@@ -92,35 +96,36 @@ public class SplashScreen extends JWindow {
         // Wait for the image to be fully loaded
         MediaTracker mediaTracker = new MediaTracker(this);
         mediaTracker.addImage(imageIcon.getImage(), 0);
-        try { mediaTracker.waitForID(0); }
-        catch(InterruptedException e) {}
+        try {
+            mediaTracker.waitForID(0);
+        } catch (InterruptedException e) {
+        }
 
         setContentPane(new JLabel(imageIcon));
 
         // Set size manually instead of using pack(), because of a bug under 1.3.1/Win32 which
         // eats a 1-pixel row of the image
-        //		pack();
+        // pack();
         int width = imageIcon.getIconWidth();
         int height = imageIcon.getIconHeight();
         setSize(width, height);
-        
+
         DialogToolkit.centerOnScreen(this);
 
         // Display the splash screen
         setVisible(true);
     }
 
-
     /**
-     * Repaints this SplashScreen to display the new given loading message, replacing the previous one. 
+     * Repaints this SplashScreen to display the new given loading message, replacing the previous one.
      *
-     * @param msg the new loading message to be displayed
+     * @param msg
+     *            the new loading message to be displayed
      */
     public void setLoadingMessage(String msg) {
         this.loadingMessage = msg;
         repaint();
     }
-
 
     /**
      * Overridden paint method.
@@ -133,24 +138,25 @@ public class SplashScreen extends JWindow {
 
         // Display loading message in the lower left corner
         int textX = LOADING_MSG_MARGIN_X;
-        int textY = getHeight()-LOADING_MSG_MARGIN_Y; 
+        int textY = getHeight() - LOADING_MSG_MARGIN_Y;
 
         g.setColor(SHADOW_TEXT_COLOR);
-        g.drawString(loadingMessage, textX-1, textY-1);
+        g.drawString(loadingMessage, textX - 1, textY - 1);
 
         g.setColor(TEXT_COLOR);
         g.drawString(loadingMessage, textX, textY);
 
         // Display version in the top right corner
         // Get FontRenderContext instance to calculate text width and height
-        java.awt.font.FontRenderContext fontRenderContext = ((Graphics2D)g).getFontRenderContext();
-        java.awt.geom.Rectangle2D textBounds = new java.awt.font.TextLayout(version, customFont, fontRenderContext).getBounds();
+        java.awt.font.FontRenderContext fontRenderContext = ((Graphics2D) g).getFontRenderContext();
+        java.awt.geom.Rectangle2D textBounds =
+                new java.awt.font.TextLayout(version, customFont, fontRenderContext).getBounds();
 
-        textX = getWidth()-(int)textBounds.getWidth()-VERSION_MARGIN_X;
-        textY = (int)textBounds.getHeight()+VERSION_MARGIN_Y;
+        textX = getWidth() - (int) textBounds.getWidth() - VERSION_MARGIN_X;
+        textY = (int) textBounds.getHeight() + VERSION_MARGIN_Y;
 
         g.setColor(SHADOW_TEXT_COLOR);
-        g.drawString(version, textX-1, textY-1);
+        g.drawString(version, textX - 1, textY - 1);
 
         g.setColor(TEXT_COLOR);
         g.drawString(version, textX, textY);
