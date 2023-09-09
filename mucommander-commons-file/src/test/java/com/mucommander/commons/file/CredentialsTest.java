db1f5454c0f65d2f6a6ff47303ee92f7974e4fd7
Arik Hadas <arik.hadas1@gmail.com>
2023-09-09 20:12:00 +0300

    simplify Credentials#getMaskedPassword

---
+++ mucommander-commons-file/src/test/java/com/mucommander/commons/file/CredentialsTest.java
@@ -0,0 +1,41 @@
+/**
+ * This file is part of muCommander, http://www.mucommander.com
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.mucommander.commons.file;
+
+import org.testng.annotations.Test;
+
+public class CredentialsTest {
+
+    @Test
+    public void testMaskedPassword() {
+        String password = "password";
+        var credentials = new Credentials("user", password);
+        var maskPassword = credentials.getMaskedPassword();
+        assert password.length() == maskPassword.length();
+        assert maskPassword.replace("*", "").isEmpty();
+    }
+
+    @Test
+    public void testMaskedAdvancedPassword() {
+        String password = "Password123!@#";
+        var credentials = new Credentials("user", password);
+        var maskPassword = credentials.getMaskedPassword();
+        assert password.length() == maskPassword.length();
+        assert maskPassword.replace("*", "").isEmpty();
+    }
+}
