8cc9f99c611542e4cc0f61167374c0ca7054c9b4
Shay Artzi <shay@artzi.org>
2025-01-02 06:28:17 -0500

    implement more smb file methods

--- mucommander-protocol-smb/src/main/java/com/mucommander/commons/file/protocol/smb/SmbjLogic.java
+++ mucommander-protocol-smb/src/main/java/com/mucommander/commons/file/protocol/smb/SmbjLogic.java
@@ -2,6 +2,6 @@ package com.mucommander.commons.file.protocol.smb;
 
 public interface SmbjLogic<T> {
 
-    T doLogic(SmbjConnectionHandler connectionHandler);
+    T doLogic(SmbjConnectionHandler connectionHandler) throws Exception;
 
 }
