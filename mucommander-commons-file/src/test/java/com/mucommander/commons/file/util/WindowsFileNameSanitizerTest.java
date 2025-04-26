c9a0c3fa8cc3b4dc496ad43d340a71a447dfc854
Shay Artzi <shay@artzi.org>
2025-04-26 16:06:28 -0400

    rename FileName to Filename

--- mucommander-commons-file/src/test/java/com/mucommander/commons/file/util/WindowsFileNameSanitizerTest.java
+++ mucommander-commons-file/src/test/java/com/mucommander/commons/file/util/WindowsFileNameSanitizerTest.java
@@ -6,11 +6,11 @@ import org.testng.annotations.Test;
 public class WindowsFileNameSanitizerTest {
 
     @Test
-    public void sanitizeFileName_whenCalledWithIllegalChars_thenSanitizeCorrectly() {
+    public void sanitizeFilename_whenCalledWithIllegalChars_thenSanitizeCorrectly() {
         String input = "report<final>*version?.txt";
         String expected = "report%3Cfinal%3E%2Aversion%3F.txt";
 
-        Assert.assertEquals(expected, WindowsFileNameSanitizer.sanitizeFileName(input));
+        Assert.assertEquals(expected, WindowsFilenameSanitizer.sanitizeFileName(input));
     }
 
 }
