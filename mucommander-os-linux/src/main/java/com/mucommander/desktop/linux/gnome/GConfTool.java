9f48682da842eb702d51187d17b6652526f187af
Arik Hadas <arik.hadas1@gmail.com>
2025-12-23 17:30:47 +0200

    code cleanup and documentation

--- mucommander-os-linux/src/main/java/com/mucommander/desktop/linux/gnome/GConfTool.java
+++ mucommander-os-linux/src/main/java/com/mucommander/desktop/linux/gnome/GConfTool.java
@@ -1,16 +1,60 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mucommander.desktop.linux.gnome;
 
 import java.io.IOException;
 
-public class GConfTool extends OSCommand {
+/**
+ * Utility class for interacting with gconftool, the legacy GNOME 2 configuration system.
+ * <p>
+ * Note: gconftool is deprecated since GNOME 3. Prefer {@link GSettings} which uses
+ * the modern dconf/gsettings system. This class is kept as a fallback for older systems.
+ * </p>
+ */
+public class GConfTool {
     private static final String COMMAND = "gconftool";
 
     private static final String GCONFTOOL_DOUBLE_CLICK_CONFIG_KEY = "/desktop/gnome/peripherals/mouse/double_click";
 
-    public static int getIntValue(String key) throws IOException, InterruptedException {
-        return runCommandWithIntReturn(COMMAND, "-g", key);
+    private GConfTool() {
+        // Utility class - prevent instantiation
     }
 
+    /**
+     * Retrieves an integer configuration value from gconftool.
+     *
+     * @param key the configuration key to retrieve
+     * @return the integer value of the configuration key
+     * @throws IOException if the command execution fails
+     * @throws InterruptedException if the command is interrupted
+     * @throws NumberFormatException if the output cannot be parsed as an integer
+     */
+    public static int getIntValue(String key) throws IOException, InterruptedException {
+        return OSCommand.runCommandWithIntReturn(COMMAND, "-g", key);
+    }
+
+    /**
+     * Retrieves the multi-click (double-click) interval from GNOME configuration.
+     *
+     * @return the double-click interval in milliseconds
+     * @throws IOException if the command execution fails
+     * @throws InterruptedException if the command is interrupted
+     */
     public static int getMultiClickInterval() throws IOException, InterruptedException {
         return getIntValue(GCONFTOOL_DOUBLE_CLICK_CONFIG_KEY);
     }
--- mucommander-os-linux/src/main/java/com/mucommander/desktop/linux/gnome/OSCommand.java
+++ mucommander-os-linux/src/main/java/com/mucommander/desktop/linux/gnome/OSCommand.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of muCommander, http://www.mucommander.com
+ *
+ * muCommander is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * muCommander is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.mucommander.desktop.linux.gnome;
 
 import org.slf4j.Logger;
@@ -6,14 +23,32 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-public class OSCommand {
+/**
+ * Utility class for executing OS commands and capturing their output.
+ * <p>
+ * This class provides methods to run external commands and retrieve their results,
+ * used by {@link GSettings} and {@link GConfTool} for GNOME configuration access.
+ * </p>
+ */
+class OSCommand {
     private static final Logger LOGGER = LoggerFactory.getLogger(OSCommand.class);
 
+    private OSCommand() {
+        // Utility class - prevent instantiation
+    }
+
+    /**
+     * Executes an OS command and returns its output.
+     *
+     * @param command the command and its arguments to execute
+     * @return the command output with newlines stripped
+     * @throws IOException if an I/O error occurs during command execution
+     * @throws InterruptedException if the current thread is interrupted while waiting
+     */
     static String runCommand(String... command) throws IOException, InterruptedException {
         ProcessBuilder pb = new ProcessBuilder(command);
         pb.directory(Paths.get(System.getProperty("user.dir")).toFile());
 
-        // Log command execution
         String commandStr = String.join(" ", command);
         LOGGER.debug("Executing: {}", commandStr);
 
@@ -21,18 +56,27 @@ public class OSCommand {
         int exitCode = process.waitFor();
 
         String output = new String(process.getInputStream().readAllBytes()).replace("\n", "").replace("\r", "");
-        String error = new String(process.getErrorStream().readAllBytes());
 
         if (exitCode != 0) {
-            LOGGER.error("Error executing command : {}", commandStr);
-            LOGGER.error("Exit code : {}", exitCode);
-            LOGGER.error("Output : {}", output);
-            LOGGER.error("Error : {}", error);
+            String error = new String(process.getErrorStream().readAllBytes());
+            LOGGER.error("Error executing command: {}", commandStr);
+            LOGGER.error("Exit code: {}", exitCode);
+            LOGGER.error("Output: {}", output);
+            LOGGER.error("Error: {}", error);
         }
 
         return output;
     }
 
+    /**
+     * Executes an OS command and returns its output as an integer.
+     *
+     * @param command the command and its arguments to execute
+     * @return the command output parsed as an integer
+     * @throws IOException if an I/O error occurs during command execution
+     * @throws InterruptedException if the current thread is interrupted while waiting
+     * @throws NumberFormatException if the output cannot be parsed as an integer
+     */
     static int runCommandWithIntReturn(String... command) throws IOException, InterruptedException {
         return Integer.parseInt(runCommand(command));
     }
